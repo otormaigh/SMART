6ca2bcef95fd9aac44ffa7dd42563aaccee22808
dgormally <david.gormally@gmail.com>
2015-01-07 12:48:17 +0000

    Update 2

--- gen/com/team3/smartapp/R.java
+++ gen/com/team3/smartapp/R.java
@@ -10,17 +10,41 @@ package com.team3.smartapp;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040002;
+        public static final int blue=0x7f040004;
+        public static final int brown=0x7f040006;
+        public static final int green=0x7f040003;
+        public static final int purple=0x7f040005;
+        public static final int red=0x7f040001;
+        public static final int white=0x7f040000;
+    }
+    public static final class dimen {
+        public static final int button_width=0x7f050009;
+        public static final int corner_radius=0x7f05000a;
+        public static final int large_font=0x7f050005;
+        public static final int medium_font=0x7f050004;
+        public static final int padding_large=0x7f050002;
+        public static final int padding_medium=0x7f050001;
+        public static final int padding_small=0x7f050000;
+        public static final int small_font=0x7f050003;
+        public static final int text_large=0x7f050008;
+        public static final int text_medium=0x7f050007;
+        public static final int text_small=0x7f050006;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int rounded_button=0x7f020001;
     }
     public static final class layout {
         public static final int activity_dublin_domino=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int title_activity_dublin_domino=0x7f040002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int label1=0x7f060002;
+        public static final int title_activity_dublin_domino=0x7f060003;
     }
     public static final class style {
         /** 
@@ -44,10 +68,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
     }
 }
