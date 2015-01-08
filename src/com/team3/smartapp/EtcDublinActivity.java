420f2874c2b318b4405eea1e1cb065745fca6a34
Elliot Tormey <elliot.tormey@gmail.com>
2015-01-08 11:58:05 +0000

    Added etc wicklow

--- src/com/team3/smartapp/EtcDublinActivity.java
+++ src/com/team3/smartapp/EtcDublinActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class EtcActivity extends Activity {
+public class EtcDublinActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_etc);
+		setContentView(R.layout.activity_etc_dublin);
 	}
 }
