82499bd9ddd140a94769b030bba4776d109128b9
MIKEY1357 <michaelcorey.devstream@gmail.com>
2015-01-08 16:18:39 +0000

    Domino Dublin fixed & Clinic Dates

---
+++ src/com/team3/smartapp/Clinic_dates_Activity.java
@@ -0,0 +1,15 @@
+package com.team3.smartapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Clinic_dates_Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_clinic_dates_);
+	}
+}
