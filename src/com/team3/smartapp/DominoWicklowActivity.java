fc3e46c39eaa4046ee0f118e078af87cf54752b2
dgormally <david.gormally@gmail.com>
2015-01-08 10:23:14 +0000

    Updated smaertapp

--- src/com/team3/smartapp/DominoWicklowActivity.java
+++ src/com/team3/smartapp/DominoWicklowActivity.java
@@ -1,19 +1,20 @@
 package com.team3.smartapp;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class DominoWicklowActivity extends Activity {
-	private Button button1;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_domino_wicklow);
 		
-		//String[] test_array = getResources().getStringArray(R.array.greystones_test);
+		Resources res = getResources();
 		
-		//button1 = (TextView)findViewById(R.id.result_text);
+		
 	}
 }
