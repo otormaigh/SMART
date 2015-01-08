1cdf9c241ea2e59469202c024759d315e2bbed5a
dgormally <david.gormally@gmail.com>
2015-01-08 15:40:34 +0000

    added logo

--- src/com/team3/smartapp/MainActivity.java
+++ src/com/team3/smartapp/MainActivity.java
@@ -3,13 +3,18 @@ package com.team3.smartapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-
+	
+	ImageView image;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		image = (ImageView) findViewById(R.id.imageView2);
 	}
 }
