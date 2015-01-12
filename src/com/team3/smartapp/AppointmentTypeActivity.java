71e12b8b5f639ec53f158918dafd6ea5fb538dec
dgormally <david.gormally@gmail.com>
2015-01-12 15:28:33 +0000

    Code added to appointmentTypes.java class

--- src/com/team3/smartapp/AppointmentTypeActivity.java
+++ src/com/team3/smartapp/AppointmentTypeActivity.java
@@ -4,12 +4,35 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class AppointmentTypeActivity extends Activity {
-
+ 
+	Button clinics;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.appointment_type);
+		addListenerOnButton();
 	}
+	
+	
+	public void addListenerOnButton() {
+		 
+		final Context context = this;
+ 
+		clinics = (Button) findViewById(R.id.clinics);
+ 
+		clinics.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+ 
+			    Intent intent = new Intent(context, activity_clinic.class);
+                startActivity(intent);   
+ 
+			}
+ 
+		});
+ 
 }
