a26c24a587edee22a4ea206e062e2071a2164171
Elliot Tormey <elliot.tormey@gmail.com>
2015-01-12 12:51:29 +0000

    formating java files

--- src/com/team3/smartapp/Clinics.java
+++ src/com/team3/smartapp/Clinics.java
@@ -14,75 +14,57 @@ public class Clinics {
 
 	public Clinics() {
 	}
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int newId) {
 		id = newId;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String newName) {
 		name = newName;
 	}
-
 	public String getAddress() {
 		return address;
 	}
-
 	public void setAddress(String newAddress) {
 		address = newAddress;
 	}
-
 	public String getOpeningHours() {
 		return openingHours;
 	}
-
 	public void setOpeningHours(String newOpeningHours) {
 		openingHours = newOpeningHours;
 	}
-
 	public String getClosingHours() {
 		return closingHours;
 	}
-
 	public void setClosingHours(String newClosingHours) {
 		closingHours = newClosingHours;
 	}
-
 	public String getRecurrence() {
 		return recurrence;
 	}
-
 	public void setRecurrence(String newRecurrence) {
 		recurrence = newRecurrence;
 	}
-
 	public String getType() {
 		return type;
 	}
-
 	public void setType(String newType) {
 		type = newType;
 	}
-
 	public int getAppointmentIntervals() {
 		return appointmentIntervals;
 	}
-
 	public void setAppointmentIntervals(int newAppointmentIntervals) {
 		appointmentIntervals = newAppointmentIntervals;
 	}
-
 	public String getDays() {
 		return days;
 	}
-
 	public void setDays(String newDays) {
 		days = newDays;
 	}		
