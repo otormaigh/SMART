12e36dc807bdb5b2e6b71f53eddc879fb4f2d24d
elliot.laptop <elliot.devstream@gmail.com>
2015-01-13 15:39:11 +0000

    updated main_activity.xml

--- src/com/team3/smartapp/AppointmentTypeActivity.java
+++ src/com/team3/smartapp/AppointmentTypeActivity.java
@@ -1,10 +1,5 @@
 package com.team3.smartapp;
 
-import com.example.midwifeoragnizer.MainActivity;
-import com.example.midwifeoragnizer.PatientActivity;
-import com.example.midwifeoragnizer.R;
-import com.example.midwifeoragnizer.MainActivity.ButtonClick;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,9 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import com.team3.smartapp.*;
-public class AppointmentTypeActivity extends Activity {
- 
+public class AppointmentTypeActivity extends Activity { 
 	Button clinics;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +29,4 @@ public class AppointmentTypeActivity extends Activity {
 				}
 			}
 		}
-}
-		
-		
- 
-	
+	}
