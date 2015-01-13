3cde463ed73f7b89c8ac12a63108dc4129266cde
dgormally <david.gormally@gmail.com>
2015-01-13 16:04:42 +0000

    Login Button renamed

--- src/com/team3/smartapp/MainActivity.java
+++ src/com/team3/smartapp/MainActivity.java
@@ -7,15 +7,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {	
-	private Button test;
+	private Button loginButton;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		test = (Button)findViewById(R.id.login);
-		test.setOnClickListener(new ButtonClick());
+		loginButton = (Button)findViewById(R.id.login);
+		loginButton.setOnClickListener(new ButtonClick());
 	}	
 	private class ButtonClick implements View.OnClickListener{
 		public void onClick(View v) {
