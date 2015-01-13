dde5dd95ffc821277214f5c10c991ac68e01f2f8
dgormally <david.gormally@gmail.com>
2015-01-13 12:34:47 +0000

    xml files now link to java files but there is an error in the androidmManifest.xml that needs fixing

--- src/com/team3/smartapp/AppointmentTypeActivity.java
+++ src/com/team3/smartapp/AppointmentTypeActivity.java
@@ -12,7 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
+import com.team3.smartapp.*;
 public class AppointmentTypeActivity extends Activity {
  
 	Button clinics;
