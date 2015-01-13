0302f398db43665092bfc2a38d3d0cc536dc9d68
dgormally <david.gormally@gmail.com>
2015-01-13 11:24:34 +0000

    mmnvn

--- src/com/team3/smartapp/MainActivity.java
+++ src/com/team3/smartapp/MainActivity.java
@@ -10,11 +10,24 @@ import android.widget.ImageView;
 public class MainActivity extends Activity {
 	
 	ImageView image;
+	private Button button1;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		image = (ImageView) findViewById(R.id.imageView2);
+		
+		button1 = (Button)findViewById(R.id.login);
+		button1.setOnClickListener(new ButtonClick());
 	}
+	
+	private class ButtonClick implements View.OnClickListener{
+		public void onClick(View v) {
+			switch(v.getId()){
+			case R.id.login:
+				Intent intentPatient = new Intent(MainActivity.this, ClinicActivity.class);
+				startActivity(intentPatient);
+				break;
+	
 }
