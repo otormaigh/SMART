27aad47507bf6a8212139f7a559d57fc3b484119
dgormally <david.gormally@gmail.com>
2015-01-14 11:34:22 +0000

    dsb

--- src/com/team3/smartapp/ClinicDatesDlaoghaire.java
+++ src/com/team3/smartapp/ClinicDatesDlaoghaire.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Clinic_Dates_Churchtown extends Activity {
+public class ClinicDatesDlaoghaire extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_clinic_dates_churchtown);
+		setContentView(R.layout.activity_clinic__dates__dlaoghaire);
 	}
 }
