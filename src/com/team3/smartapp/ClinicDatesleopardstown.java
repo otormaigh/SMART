3ede70eda5a65d56f06d5cd937b785165aa5060d
MIKEY1357 <michaelcorey.devstream@gmail.com>
2015-01-14 11:59:28 +0000

    Clinic Dates

--- src/com/team3/smartapp/ClinicDatesleopardstown.java
+++ src/com/team3/smartapp/ClinicDatesleopardstown.java
@@ -2,14 +2,12 @@ package com.team3.smartapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class Clinic_Dates_Dlaoghaire extends Activity {
+public class ClinicDatesleopardstown extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_clinic__dates__dlaoghaire);
+		setContentView(R.layout.activity_clinic_dates_leopardstown);
 	}
 }
