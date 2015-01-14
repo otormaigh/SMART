fbaeb125ba967e26fb8006ec1a044de4a471dd0e
Elliot Tormey <elliot.tormey@gmail.com>
2015-01-14 09:56:31 +0000

    added files

---
+++ src/com/team3/smartapp/Appointments.java
@@ -0,0 +1,64 @@
+package com.team3.smartapp;
+
+public class Appointments {
+	private String id;
+	private String date;
+	private String time;
+	private String serviceProvderId;
+	private String serviceUserId;
+	private boolean priority;
+	private String vistType;
+	private String serviceOptionId;
+	
+
+	public Appointments (){	
+	}
+	public String getId(){ 
+		return id;
+	}	
+	public String getDate(){
+		return date;
+	}	
+	public String getTime(){
+		return time;
+	}	
+	public String getServiceProvderrId(){
+		return serviceProvderId;
+	}	
+	public String getServiceUserId(){
+		return serviceUserId;
+	}	
+	public Boolean getPriority(){
+		return priority;
+	}
+	public String getVistType(){
+		return vistType;
+	}
+	public String getServiceOptionId(){
+		return serviceOptionId;
+	}	
+	public void setId( String newId){
+		this.id = newId;
+	}	
+	public void setDate( String newDate){
+		this.date = newDate;
+	}	
+	public void settime( String newTime){
+		this.time = newTime;
+	}	
+	public void setserviceProvderId( String newServiceProvderId){
+		this.serviceProvderId = newServiceProvderId;
+	}
+	public void setServiceUserId( String serviceUserId){
+		this.serviceUserId = serviceUserId;
+	}	
+	public void setPriority( boolean Priority){
+		this.priority = Priority;
+	}
+	public void setVistType( String VistType){
+		this.vistType = VistType;
+	}	
+	public void setServiceOptionId (String ServiceOptionId){
+		this.serviceOptionId = ServiceOptionId;
+	}
+}
