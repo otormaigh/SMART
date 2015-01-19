77a1e17fe2c99b39f2cfda90e9ffa2329eaeeed1
MIKEY1357 <michaelcorey.devstream@gmail.com>
2015-01-19 12:15:12 +0000

    dimen edited

--- src/com/team3/smartapp/ClinicActivity.java
+++ src/com/team3/smartapp/ClinicActivity.java
@@ -1,9 +1,11 @@
 
 package com.team3.smartapp;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -34,6 +36,12 @@ public class ClinicActivity extends Activity {
 		
 		satellite = (Button)findViewById(R.id.satellite);
 		satellite.setOnClickListener(new ButtonClick());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	private class ButtonClick implements View.OnClickListener{
