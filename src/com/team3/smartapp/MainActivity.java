2adc44e3771bb972663d537d47941bf4a2807d8d
dgormally <david.gormally@gmail.com>
2015-01-19 12:14:03 +0000

    d3e3x

--- src/com/team3/smartapp/MainActivity.java
+++ src/com/team3/smartapp/MainActivity.java
@@ -4,6 +4,9 @@ package com.team3.smartapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,6 +22,9 @@ public class MainActivity extends Activity {
 		loginButton = (Button)findViewById(R.id.login);
 		loginButton.setOnClickListener(new ButtonClick());
 	}	
+	
+
+
 	private class ButtonClick implements View.OnClickListener{
 		public void onClick(View v) {
 			switch(v.getId()){
