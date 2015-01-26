955677d9e1d5fb3411db54f1a64c66e15ee6e2e4
elliot.laptop <elliot.devstream@gmail.com>
2015-01-26 16:11:07 +0000

    updated MainActivity.java

--- src/com/team3/smartapp/MainActivity.java
+++ src/com/team3/smartapp/MainActivity.java
@@ -133,6 +133,7 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onProgressUpdate(Void... values) {
+        	Log.d("MYLOG", "On progress update");
         }
 	}
 
