db2202b7f796c21a58ae9f64fecbee1303e2c77c
elliot.laptop <elliot.devstream@gmail.com>
2015-02-11 09:51:21 +0000

    added show appointment for specific date depending on spinner selected

--- src/connecttodb/GetToken.java
+++ src/connecttodb/GetToken.java
@@ -6,23 +6,29 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.HashMap;
 import models.Login_model;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.util.Log;
 
 public class GetToken {
-	private String username;
-	private String password;
-	private HttpURLConnection httpcon;
+	private String username, password, strToken, st;
 	private String loginURL = "http://54.72.7.91:8888/login";
-	public Login_model login = new Login_model();
 	private String encodeType = "ISO-8859-1";
-	JSONObject credentials = new JSONObject();
-	JSONObject jsonLogin = new JSONObject();
+	private String responseCode = null;
+	private int id, ch;
+	private HttpURLConnection httpcon;
+	private Login_model login = new Login_model();
+	private JSONObject credentials = new JSONObject();
+	private JSONObject jsonLogin = new JSONObject();
+	private JSONObject json = new JSONObject();
+	private InputStream is;
+	private OutputStream os;
+	private StringBuffer sb;
 	
 	public String getToken(String username, String password){
+		this.username = username;
+		this.password = password;
 		return getToken();
 	}	
 	private String getToken(){
@@ -30,6 +36,7 @@ public class GetToken {
 			credentials.put("username", username);
 			credentials.put("password", password);
 			jsonLogin.put("login", credentials);
+			Log.d("MYLOG", "jsonLogin: " + jsonLogin);
 			
 			httpcon = (HttpURLConnection) ((new URL(loginURL).openConnection()));
 			URLEncoder.encode(loginURL, encodeType);
@@ -42,24 +49,26 @@ public class GetToken {
 			// form request
 			byte[] inputBytes = jsonLogin.toString().getBytes(encodeType);
 
-			OutputStream os = httpcon.getOutputStream();
+			os = httpcon.getOutputStream();
 			os.write(inputBytes);
 			os.flush();
 			os.close();
 			
 			// grab the response
-			InputStream is = httpcon.getInputStream();
-			int ch;
-			StringBuffer sb = new StringBuffer();
+			is = httpcon.getInputStream();
+			sb = new StringBuffer();
 			while ((ch = is.read()) != -1) {
 				sb.append((char) ch);
 			}
-			String st = sb.toString();
+			st = sb.toString();
 
 			// create JSON Object to get Token using token key
-			JSONObject json = new JSONObject(st);
-			String strToken = (String) ((JSONObject) json.get("login")).get("token");
+			json = new JSONObject(st);
+			strToken = (String) ((JSONObject) json.get("login")).get("token");
+			id = (Integer) ((JSONObject) json.get("login")).get("id");
+			login.setUserId(id);
 			
+			Log.d("MYLOG", "id: " + Login_model.getUserId());
 			Log.d("MYLOG", "sb.toString(): " + sb.toString());
 			Log.d("MYLOG", "Response Code: " + httpcon.getResponseCode());
 			
@@ -70,8 +79,7 @@ public class GetToken {
 		}
 		return null;
 	}	
-	public String getResponseCode(){
-		String responseCode = null;
+	public String getResponseCode(){		
 		try {
 			responseCode = String.valueOf(httpcon.getResponseCode());
 			Log.d("MYLOG", "Response Code: " + httpcon.getResponseCode());
