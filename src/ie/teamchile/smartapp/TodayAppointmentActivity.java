9b32905c344cde8d3e0b7d822b2bf1079970fd44
jomo45 <joemurray.devstream@gmail>
2015-02-19 14:15:35 +0000

    add todayappointmentactiviy

---
+++ src/ie/teamchile/smartapp/TodayAppointmentActivity.java
@@ -0,0 +1,46 @@
+package ie.teamchile.smartapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class TodayAppointmentActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_today_appointment);
+		ListView listView = (ListView)findViewById(R.id.list);
+		// declare and initialize a String array with OS names
+		String [] osNames = 
+		new String []{"Lolli-pop", "Ice-Cream Sandwich", "Kit-Kat", "Eclair", "Choc-Ice", "Gingerbread"};
+		
+		// create an adapter of type String to populate the listview
+				ArrayAdapter<String>adapter =
+		            new ArrayAdapter<String>(this,
+			         R.layout.activity_today_appointment, R.id.text_1, osNames);
+		    
+				listView.setAdapter(adapter);
+				listView.setOnItemClickListener((android.widget.AdapterView.OnItemClickListener) this);
+		    }
+
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				String[] osNames = null;
+				// TODO Auto-generated method stub
+				Log.d("Item Listener", osNames[position]);
+				
+	
+	
+	
+	
+	
+	}
+}
+
