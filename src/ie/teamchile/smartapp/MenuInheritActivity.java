c41359607e957cdeccece92a9b718a9a8e4b33e8
dgormally <david.gormally@gmail.com>
2015-02-19 23:04:00 +0000

    Code cleanUp on menuInheritActvity

--- src/ie/teamchile/smartapp/MenuInheritActivity.java
+++ src/ie/teamchile/smartapp/MenuInheritActivity.java
@@ -34,7 +34,7 @@ public class MenuInheritActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
             case R.id.menu_item1:
-            	//logout
+           //logout
            new AlertDialog.Builder(this)
            .setTitle(R.string.Logout_title)
            .setMessage(R.string.Logout_dialog_message)
@@ -47,30 +47,28 @@ public class MenuInheritActivity extends Activity {
               }).setPositiveButton(R.string.Yes, new DialogInterface.OnClickListener()
         {
         public void onClick(DialogInterface dialoginterface, int i)
-		              {
-		                 Log.d("MYLOG", "Logout button pressed");
-		                 if(Login_model.getToken() == ""){
-		                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-		                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | 
-		                 Intent.FLAG_ACTIVITY_CLEAR_TASK |
-		                 Intent.FLAG_ACTIVITY_NEW_TASK);
-		                 	startActivity(intent);        			
-		                 	}else {
-		                 		
-		                 		  logout.doLogout(Login_model.getToken());
-		                 		   //login.setToken(null);        			
-		                 		  Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-		                 		  intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | 
-		                 	      Intent.FLAG_ACTIVITY_CLEAR_TASK |
-		                 	      Intent.FLAG_ACTIVITY_NEW_TASK);
-		                 		  Toast.makeText(getApplicationContext(), "You are now logged out", Toast.LENGTH_SHORT).show();
-		                 		  startActivity(intent);
-		                 	}
-		              }
-		               }
-                 )
-		             
-              .show();
+        {
+		  Log.d("MYLOG", "Logout button pressed");
+		  if(Login_model.getToken() == ""){
+		  Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+		  intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | 
+		  Intent.FLAG_ACTIVITY_CLEAR_TASK |
+		  Intent.FLAG_ACTIVITY_NEW_TASK);
+		  startActivity(intent);
+		}
+		   else {      		
+		        logout.doLogout(Login_model.getToken());
+		         //login.setToken(null);        			
+		        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+		        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | 
+		        Intent.FLAG_ACTIVITY_CLEAR_TASK |
+		        Intent.FLAG_ACTIVITY_NEW_TASK);
+		        Toast.makeText(getApplicationContext(), "You are now logged out", Toast.LENGTH_SHORT).show();
+		        startActivity(intent);
+		   }
+        }
+        })
+         .show();
 
             case R.id.menu_item2:
                 System.out.println("Item 2 was selected!");
