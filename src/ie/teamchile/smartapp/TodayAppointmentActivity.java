e24be052c128d980427b6932516458c0e3b31c9c
Elliot Tormey <elliot.tormey@gmail.com>
2015-02-19 14:36:03 +0000

    update TodayAppointmentActivity

--- src/ie/teamchile/smartapp/TodayAppointmentActivity.java
+++ src/ie/teamchile/smartapp/TodayAppointmentActivity.java
@@ -1,16 +1,13 @@
 package ie.teamchile.smartapp;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class TodayAppointmentActivity extends Activity {
+public class TodayAppointmentActivity extends MenuInheritActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +19,14 @@ public class TodayAppointmentActivity extends Activity {
 		new String []{"Lolli-pop", "Ice-Cream Sandwich", "Kit-Kat", "Eclair", "Choc-Ice", "Gingerbread"};
 		
 		// create an adapter of type String to populate the listview
-				ArrayAdapter<String>adapter =
-		            new ArrayAdapter<String>(this,
-			         R.layout.activity_today_appointment, R.id.text_1, osNames);
-		    
-				listView.setAdapter(adapter);
-				listView.setOnItemClickListener((android.widget.AdapterView.OnItemClickListener) this);
-		    }
-
-			public void onItemClick(AdapterView<?> parent, View view, int position,
-					long id) {
-				String[] osNames = null;
-				// TODO Auto-generated method stub
-				Log.d("Item Listener", osNames[position]);
-				
-	
-	
-	
-	
-	
+		ArrayAdapter<String>adapter = new ArrayAdapter<String>(this,
+									R.layout.activity_today_appointment,
+									R.id.text_1, osNames);
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener((android.widget.AdapterView.OnItemClickListener) this);
+	}
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		String[] osNames = null;
+		Log.d("Item Listener", osNames[position]);
 	}
 }
-
