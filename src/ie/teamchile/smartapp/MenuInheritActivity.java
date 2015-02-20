2078c8402a0c5d2448c5debadabf9d58055a860e
elliot.laptop <elliot.devstream@gmail.com>
2015-02-20 10:17:23 +0000

    change colour of custom dialog border

--- src/ie/teamchile/smartapp/MenuInheritActivity.java
+++ src/ie/teamchile/smartapp/MenuInheritActivity.java
@@ -2,6 +2,7 @@
 package ie.teamchile.smartapp;
 
 import models.Login_model;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.PendingIntent;
@@ -14,6 +15,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
 import connecttodb.Logout;
 
 public class MenuInheritActivity extends Activity {
@@ -24,55 +26,52 @@ public class MenuInheritActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
-    }    
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.menu_item1:
-           //logout
-           new AlertDialog.Builder(this)
-           .setTitle(R.string.Logout_title)
-           .setMessage(R.string.Logout_dialog_message)
-           .setNegativeButton(R.string.No,
-            new DialogInterface.OnClickListener()
-              {
-              	public void onClick(DialogInterface dialoginterface, int i)
-              	{              		
-              	}
-              }).setPositiveButton(R.string.Yes, new DialogInterface.OnClickListener()
-        {
-        public void onClick(DialogInterface dialoginterface, int i)
-        {
-		  Log.d("MYLOG", "Logout button pressed");
-		  if(Login_model.getToken() == ""){
-		  Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-		  intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | 
-		  Intent.FLAG_ACTIVITY_CLEAR_TASK |
-		  Intent.FLAG_ACTIVITY_NEW_TASK);
-		  startActivity(intent);
-		}
-		   else {      		
-		        logout.doLogout(Login_model.getToken());
-		         //login.setToken(null);        			
-		        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-		        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | 
-		        Intent.FLAG_ACTIVITY_CLEAR_TASK |
-		        Intent.FLAG_ACTIVITY_NEW_TASK);
-		        Toast.makeText(getApplicationContext(), "You are now logged out", Toast.LENGTH_SHORT).show();
-		        startActivity(intent);
-		   }
-        }
-        })
-         .show();
+                //logout
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.Logout_title)
+                        .setMessage(R.string.Logout_dialog_message)
+                        .setNegativeButton(R.string.No,
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialoginterface, int i) {
+                                    }
+                                }).setPositiveButton(R.string.Yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialoginterface, int i) {
+                        Log.d("MYLOG", "Logout button pressed");
+                        if (Login_model.getToken() == "") {
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                                    Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                                    Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        } else {
+                            logout.doLogout(Login_model.getToken());
+                            //login.setToken(null);
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                                    Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                                    Intent.FLAG_ACTIVITY_NEW_TASK);
+                            Toast.makeText(getApplicationContext(), "You are now logged out", Toast.LENGTH_SHORT).show();
+                            startActivity(intent);
+                        }
+                    }
+                })
+                        .show();
 
             case R.id.menu_item2:
                 System.out.println("Item 2 was selected!");
         }
-        return super.onOptionsItemSelected(item); 
+        return super.onOptionsItemSelected(item);
     }
 }
