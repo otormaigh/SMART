138b3b3953a2be55d3269eee51518e3f2dcc7b13
dgormally <david.gormally@gmail.com>
2015-02-20 12:52:42 +0000

    Updated PostNatal and AntiNatal, unwanted code removed

--- src/ie/teamchile/smartapp/PostNatal.java
+++ src/ie/teamchile/smartapp/PostNatal.java
@@ -1,34 +1,14 @@
 package ie.teamchile.smartapp;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class PostNatal extends Activity {
+public class PostNatal extends MenuInheritActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_post_natal);
+		setContentView(R.layout.activity_service_user);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.post_natal, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 }
