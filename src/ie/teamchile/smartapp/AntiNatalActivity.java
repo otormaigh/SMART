7902eea88a047dfe552f5945ed24625608d68caf
MIKEY1357 <michaelcorey.devstream@gmail.com>
2015-02-26 12:36:43 +0000

    Changed post natal to anti natal

--- src/ie/teamchile/smartapp/AntiNatalActivity.java
+++ src/ie/teamchile/smartapp/AntiNatalActivity.java
@@ -1,12 +1,40 @@
 package ie.teamchile.smartapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TableRow;
 
 public class AntiNatalActivity extends MenuInheritActivity {
+	TableRow row1;
+	TableRow row2;
+	TableRow row3;
+	TableRow row4;
+	TableRow row5;
+	TableRow row6;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_anti_natal);
+		row5 = (TableRow) findViewById(R.id.button_parity);
+		row6 = (TableRow) findViewById(R.id.button_obshistory);
+        
+        row5.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Log.e("Click ", "Row 5");
+        	  }
+        });
+        
+        row6.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Log.e("Click ", "Row 6");
+        	  }
+        });
 	}
 }
