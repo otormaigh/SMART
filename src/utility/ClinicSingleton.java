acdbc6f010a872d5d7560c567596699e97541257
Elliot Tormey <elliot.tormey@gmail.com>
2015-02-26 22:37:49 +0000

    add comments on methods

--- src/utility/ClinicSingleton.java
+++ src/utility/ClinicSingleton.java
@@ -13,23 +13,16 @@ import android.util.Log;
 
 public class ClinicSingleton {	
 	private static ClinicSingleton singleInstance;
-	private JSONArray clinicsArray = new JSONArray();
-	private HashMap<String, String> idHash = new HashMap<String, String>();
-	private ArrayList<String> idList;
+	private HashMap<String, String> idHash;
+	private ArrayList<JSONObject> jsonValues;
 	private AccessDBTable db = new AccessDBTable();
 	private String response;
 	private JSONArray query;
 	private JSONObject jsonNew;
 
-	private int id;
-	private String name;
-	private String address;
-	private String openingHours;
-	private String closingHours;
-	private String recurrence;
-	private String type;
-	private int appointmentIntervals;
-	private String days;
+	private int id, appointmentIntervals;
+	private String name, address, openingHour, closingHour,
+				   recurrence, type, days;
 
 	private ClinicSingleton() {
 	}	
@@ -72,15 +65,15 @@ public class ClinicSingleton {
 		return idHash;
 	}
 	public void setHashMapofIdClinic(JSONArray clinicArray) {
-		ArrayList<JSONObject> jsonValues = new ArrayList<JSONObject>();
-		HashMap<String, String> idHash = new HashMap<String, String>();
+		jsonValues = new ArrayList<JSONObject>();
+		idHash = new HashMap<String, String>();
 		String id; // key
 		String clinic; // value
 		
 		try {
-			for (int i = 0; i < clinicArray.length(); i++) {
-				jsonValues.add(clinicArray.getJSONObject(i));
-			}	
+			/*for (int i = 0; i < clinicArray.length(); i++) {		//possibly redundant with iterator 
+				jsonValues.add(clinicArray.getJSONObject(i));		//after this need to test
+			}	*/
 			for (int i = 0; i < jsonValues.size(); i++) {
 				id = String.valueOf((jsonValues.get(i).getInt("id")));
 				clinic = jsonValues.get(i).toString();			
@@ -90,15 +83,11 @@ public class ClinicSingleton {
 			e.printStackTrace();
 		}
 		Log.d("singleton", "idhash of clinics: " + idHash);
-		this.idHash = idHash;
+		//this.idHash = idHash;
 	}
 
 	public int getId() {
 		return id;
-	}
-
-	public void setId(int newId) {
-		id = newId;
 	}
 
 	public String getName(String idToSearch) {
@@ -106,27 +95,18 @@ public class ClinicSingleton {
 		 *  helper method to give back the name of the clinic
 		 *  in the ID's hashmap
 		 */
-		String name = "";
-		JSONObject jsondude;		
+		JSONObject json;		
 		try {
-			jsondude = new JSONObject(idHash.get(idToSearch));
-			name = jsondude.get("name").toString();	
+			json = new JSONObject(idHash.get(idToSearch));
+			name = json.get("name").toString();	
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 		return name;
 	}
 
-	public void setName(String newName) {
-		name = newName;
-	}
-
 	public String getAddress() {
 		return address;
-	}
-
-	public void setAddress(String newAddress) {
-		address = newAddress;
 	}
 
 	public String getOpeningHours(String idToSearch) {
@@ -134,66 +114,40 @@ public class ClinicSingleton {
 		 * helper to get the opening times from the 
 		 * HashMap of ID's
 		 */
-		String openingHour = "";
-		JSONObject jsondude;		
+		JSONObject json;		
 		try {
-			jsondude = new JSONObject(idHash.get(idToSearch));
-			openingHour = jsondude.get("opening_time").toString();	
+			json = new JSONObject(idHash.get(idToSearch));
+			openingHour = json.get("opening_time").toString();	
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 		return openingHour;
 	}
 
-	public void setOpeningHours(String newOpeningHours) {
-		openingHours = newOpeningHours;
-	}
-
 	public String getClosingHours(String idToSearch) {
-		String closingHour = "";
-		JSONObject jsondude;		
+		JSONObject json;		
 		try {
-			jsondude = new JSONObject(idHash.get(idToSearch));
-			closingHour = jsondude.get("closing_time").toString();	
+			json = new JSONObject(idHash.get(idToSearch));
+			closingHour = json.get("closing_time").toString();	
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 		return closingHour;
 	}
 
-	public void setClosingHours(String newClosingHours) {
-		closingHours = newClosingHours;
-	}
-
 	public String getRecurrence() {
 		return recurrence;
-	}
-
-	public void setRecurrence(String newRecurrence) {
-		recurrence = newRecurrence;
 	}
 
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String newType) {
-		type = newType;
-	}
-
 	public int getAppointmentIntervals() {
 		return appointmentIntervals;
 	}
 
-	public void setAppointmentIntervals(int newAppointmentIntervals) {
-		appointmentIntervals = newAppointmentIntervals;
-	}
-
 	public String getDays() {
 		return days;
-	}
-
-	public void setDays(String newDays) {
-		days = newDays;
 	}
 }
