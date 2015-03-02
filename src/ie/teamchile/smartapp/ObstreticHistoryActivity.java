e58270635c519126fb03962331ca9a067a42b252
MIKEY1357 <michaelcorey.devstream@gmail.com>
2015-03-02 11:38:36 +0000

    Added Activity to class names

--- src/ie/teamchile/smartapp/ObstreticHistoryActivity.java
+++ src/ie/teamchile/smartapp/ObstreticHistoryActivity.java
@@ -2,11 +2,14 @@ package ie.teamchile.smartapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class SubMenu extends Activity {
+public class ObstreticHistoryActivity extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_obstretic_history);
 	}
 }
