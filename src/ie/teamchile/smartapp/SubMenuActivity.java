f9684085059a4f896be10a507b1b576e0bc803f6
MIKEY1357 <michaelcorey.devstream@gmail.com>
2015-03-02 11:38:36 +0000

    Added Activity to class names

--- src/ie/teamchile/smartapp/SubMenuActivity.java
+++ src/ie/teamchile/smartapp/SubMenuActivity.java
@@ -2,14 +2,11 @@ package ie.teamchile.smartapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class StockNote extends Activity {
-
+public class SubMenuActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_stock_note);
+		setContentView(R.layout.activity_main);
 	}
 }
