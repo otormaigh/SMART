9eb1940c2ebc4ce1d9c08a5d7be6b076e44df891
MIKEY1357 <michaelcorey.devstream@gmail.com>
2015-03-02 15:23:41 +0000

    Changed Obstretic history to obstetric history

--- src/ie/teamchile/smartapp/ObstetricHistoryActivity.java
+++ src/ie/teamchile/smartapp/ObstetricHistoryActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ObstreticHistoryActivity extends Activity {
+public class ObstetricHistoryActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_obstretic_history);
+		setContentView(R.layout.activity_obstetric_history);
 	}
 }
