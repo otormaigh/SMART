1ad8bce6f5612ac0177185b86ec93e351f0770ea
elliot.laptop <elliot.devstream@gmail.com>
2015-03-03 16:25:49 +0000

    confirm appointment view name, date and time populated depending on appoitnment ID selected

--- src/ie/teamchile/smartapp/ConfirmAppointmentActivity.java
+++ src/ie/teamchile/smartapp/ConfirmAppointmentActivity.java
@@ -1,12 +1,22 @@
 package ie.teamchile.smartapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ConfirmAppointmentActivity extends MenuInheritActivity {
+	private TextView userName, apptTime, apptDate;
+	String details;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_appointment);
+        userName = (TextView)findViewById(R.id.service_user_db);
+        apptTime = (TextView)findViewById(R.id.appointment_time_db);
+        apptDate = (TextView)findViewById(R.id.appointment_date_db);
+        
+        userName.setText(getIntent().getStringExtra("name"));
+        apptTime.setText(getIntent().getStringExtra("time"));
+        apptDate.setText(getIntent().getStringExtra("date"));
     }
 }
