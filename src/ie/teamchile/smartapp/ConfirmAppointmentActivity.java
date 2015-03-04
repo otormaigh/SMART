10fed4332f45dc42b564f5f72edc67146e7e6034
Elliot Tormey <elliot.tormey@gmail.com>
2015-03-04 12:56:50 +0000

    confirm appointment all populated when click on appointment in list of appointmnets

--- src/ie/teamchile/smartapp/ConfirmAppointmentActivity.java
+++ src/ie/teamchile/smartapp/ConfirmAppointmentActivity.java
@@ -1,22 +1,34 @@
 package ie.teamchile.smartapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class ConfirmAppointmentActivity extends MenuInheritActivity {
-	private TextView userName, apptTime, apptDate;
-	String details;
+	private TextView userName, hospitalNumber, appointmentClinic, apptDate, apptTime,
+					 duration, email, sms;
+	private String details;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_appointment);
         userName = (TextView)findViewById(R.id.service_user_db);
-        apptTime = (TextView)findViewById(R.id.appointment_time_db);
+        hospitalNumber = (TextView) findViewById(R.id.hospital_number_db);
+        appointmentClinic = (TextView) findViewById(R.id.appointment_clinic_db);
         apptDate = (TextView)findViewById(R.id.appointment_date_db);
+        apptTime = (TextView)findViewById(R.id.appointment_time_db);
+        duration = (TextView)findViewById(R.id.duration_db);
+        email = (TextView)findViewById(R.id.email_to_db);
+        sms = (TextView)findViewById(R.id.sms_to_db);
         
         userName.setText(getIntent().getStringExtra("name"));
-        apptTime.setText(getIntent().getStringExtra("time"));
+        hospitalNumber.setText(getIntent().getStringExtra("hospitalNumber"));
+        appointmentClinic.setText(getIntent().getStringExtra("clinicName"));
         apptDate.setText(getIntent().getStringExtra("date"));
+        apptTime.setText(getIntent().getStringExtra("time"));
+        duration.setText(getIntent().getStringExtra("duration") + " minutes");
+        email.setText(getIntent().getStringExtra("email"));
+        sms.setText(getIntent().getStringExtra("mobile"));
     }
 }
