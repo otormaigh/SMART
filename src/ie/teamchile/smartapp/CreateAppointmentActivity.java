96b27cb37b0c5fa2ab0d85368e146acab6e209ad
Elliot Tormey <elliot.tormey@gmail.com>
2015-03-05 10:45:31 +0000

    fixed errors

---
+++ src/ie/teamchile/smartapp/CreateAppointmentActivity.java
@@ -0,0 +1,15 @@
+package ie.teamchile.smartapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CreateAppointmentActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_appointment);
+	}
+}
