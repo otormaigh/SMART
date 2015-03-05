67baec60c9834fa68dae73913d84218adfed66a6
dgormally <david.gormally@gmail.com>
2015-03-05 16:24:41 +0000

    Populating service user with ServiceUserSingleteon

--- src/ie/teamchile/smartapp/ServiceUserSearchActivity.java
+++ src/ie/teamchile/smartapp/ServiceUserSearchActivity.java
@@ -8,7 +8,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
+import utility.ServiceUserSingleton;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -35,6 +37,11 @@ public class ServiceUserSearchActivity extends MenuInheritActivity {
 	JSONObject json;
 	JSONArray query, query2, query3;
 	AccessDBTable dbTable = new AccessDBTable();
+	private ProgressDialog pd;
+	private Intent intent;
+	private String response;
+	private JSONObject jsonNew;
+	private AccessDBTable db = new AccessDBTable();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,27 +64,11 @@ public class ServiceUserSearchActivity extends MenuInheritActivity {
 			case R.id.search:
 				Log.d("MYLOG", "Search Button Pressed");
 				enteredSearch = searchParams.getText().toString();
-				new LongOperation().execute((String[]) null);
+				new LongOperation(ServiceUserSearchActivity.this).execute("service_users?name=" + enteredSearch);
 				break;
 			case R.id.search_result_1:
 				Log.d("MYLOG", "First Result Button Pressed");
 				Intent intent = new Intent(ServiceUserSearchActivity.this, ServiceUserActivity.class);
-				intent.putExtra("hospital_number", hospitalNumber);
-                intent.putExtra("name", name);
-                intent.putExtra("dob", dob);
-                intent.putExtra("gestation", gestation);
-                intent.putExtra("parity", parity);
-                intent.putExtra("email", email);
-				intent.putExtra("mobile_number", mobileNumber);
-				intent.putExtra("road", road);
-				intent.putExtra("county", county);
-				intent.putExtra("post_code", postCode);
-				intent.putExtra("next_of_kin_name", nextOfKinName);
-				intent.putExtra("next_of_kin_phone", nextOfKinContactNumber);
-				intent.putExtra("deliveryDate", deliveryDate);
-				intent.putExtra("bloodGroup", bloodGroup);
-				intent.putExtra("rhesus", rhesus);
-                intent.putExtra("obstetricHistory", obstetricHistory);
 		        startActivity(intent);
 				break;
 			case R.id.search_result_2:
@@ -87,88 +78,69 @@ public class ServiceUserSearchActivity extends MenuInheritActivity {
 			}
 		}
 	}
-	public int getObjects(JSONArray obj, String key, String val) {
-		for(int i = 0; i < obj.length(); i++){
-			try {
-				if(((JSONObject) ((JSONObject) obj.get(i)).get("personal_fields")).get(key).equals(val)){
-					return i;
-				}
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
+
+	public class LongOperation extends AsyncTask<String, Void, JSONObject> {
+		private Context context;
+		LongOperation(Context context){
+			this.context = context;
 		}
-	    return 0;
-	}
-	public class LongOperation extends AsyncTask<String, Void, String> {
 		@Override
 		protected void onPreExecute() {
+			pd = new ProgressDialog(context);
+            pd.setMessage("Fetching Information");
+            pd.show();
 		}
-		protected String doInBackground(String... params) {
+		protected JSONObject doInBackground(String... params) {
 			Log.d("MYLOG", "ServiceUserSearch DoInBackground");
-			String dbQuery = dbTable.accessDB("service_users");
+			String dbQuery = dbTable.accessDB(params[0]);
 			try {
 				json = new JSONObject(dbQuery);
-				query = json.getJSONArray("service_users");
-				query2 = json.getJSONArray("pregnancies");
+				//query = json.getJSONArray("service_users");
+				//query2 = json.getJSONArray("pregnancies");
 				
-				arrayPos = getObjects(query, "name", enteredSearch);
+				//arrayPos = getObjects(query, "name", enteredSearch);
 
-				first = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("name")).toString();
-				hospitalNumber = (((JSONObject) query.get(arrayPos)).get("hospital_number")).toString();
-                name = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("name")).toString();
-                dob = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("dob")).toString();
-				email = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("email")).toString();
-				mobileNumber = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("mobile_phone")).toString();
-				road = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("home_address")).toString();
-				county = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("home_county")).toString();
-				postCode = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("home_post_code")).toString();
-				nextOfKinName = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("next_of_kin_name")).toString();
-				nextOfKinContactNumber = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("next_of_kin_phone")).toString();
+				//first = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("name")).toString();
+				//hospitalNumber = (((JSONObject) query.get(arrayPos)).get("hospital_number")).toString();
+                //name = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("name")).toString();
+                //dob = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("dob")).toString();
+				//email = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("email")).toString();
+				//mobileNumber = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("mobile_phone")).toString();
+				//road = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("home_address")).toString();
+				//county = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("home_county")).toString();
+				//postCode = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("home_post_code")).toString();
+				//nextOfKinName = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("next_of_kin_name")).toString();
+				//nextOfKinContactNumber = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("personal_fields")).get("next_of_kin_phone")).toString();
 
-				gestation = ((JSONObject) query2.get(0)).get("gestation").toString();
-				deliveryDate = ((JSONObject) query2.get(0)).get("estimated_delivery_date").toString();
+				//gestation = ((JSONObject) query2.get(0)).get("gestation").toString();
+				//deliveryDate = ((JSONObject) query2.get(0)).get("estimated_delivery_date").toString();
+				//String parity = ServiceUserSingleton.getSingletonIntance().getParity(query.get(arrayPos));
 				
-				
-				parity = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("clinical_fields")).get("parity")).toString();
-				bloodGroup = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("clinical_fields")).get("blood_group")).toString();
-				rhesus = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("clinical_fields")).get("rhesus")).toString();
-				obstetricHistory = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("clinical_fields")).get("previous_obstetric_history")).toString();
-
-
-			
+				//bloodGroup = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("clinical_fields")).get("blood_group")).toString();
+				//rhesus = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("clinical_fields")).get("rhesus")).toString();
+				//obstetricHistory = (((JSONObject) ((JSONObject) query.get(arrayPos)).get("clinical_fields")).get("previous_obstetric_history")).toString();
 
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
-			return null;
+			return json;
 		}
 		@Override
 		protected void onProgressUpdate(Void... values) {
 			Log.d("MYLOG", "On progress update");
 		}
 		@Override
-		protected void onPostExecute(String result) {
+		protected void onPostExecute(JSONObject result) {
             Log.d("MYLOG", "onPostExecute");
-            searchResult1.setText(first);
-            ServiceUserSearchActivity.this.hospitalNumber = hospitalNumber.toString();
-            ServiceUserSearchActivity.this.name = name.toString();
-            ServiceUserSearchActivity.this.dob = dob.toString();
-            ServiceUserSearchActivity.this.email = email.toString();
-            ServiceUserSearchActivity.this.mobileNumber = mobileNumber.toString();
-            ServiceUserSearchActivity.this.road = road.toString();
-            ServiceUserSearchActivity.this.county = county.toString();
-            ServiceUserSearchActivity.this.postCode = postCode.toString();
-            ServiceUserSearchActivity.this.nextOfKinName = nextOfKinName.toString();
-            ServiceUserSearchActivity.this.nextOfKinContactNumber = nextOfKinContactNumber.toString();
-            ServiceUserSearchActivity.this.gestation = gestation.toString();
-            ServiceUserSearchActivity.this.parity=parity.toString();
-            ServiceUserSearchActivity.this.deliveryDate=deliveryDate.toString();
-            ServiceUserSearchActivity.this.bloodGroup=bloodGroup.toString();
-            ServiceUserSearchActivity.this.rhesus=rhesus.toString();
-            ServiceUserSearchActivity.this.obstetricHistory=obstetricHistory.toString();
-
-
-            
+            ServiceUserSingleton.getSingletonIntance().setPatientInfo(result);
+            pd.dismiss();
+            /*
+			 * if result from database is empty (chcek if null) toast to say no query found
+			 * if not empty do getSinglton.getName
+			 * set this to button text
+			 */
+            //searchResult1.setText(ServiceUserSingleton.getSingltetonInstance().getName());
+           
 		}
 	}
 }
