b566d5e31516c5f14f437eb2b5bde335ab12c660
Elliot Tormey <elliot.tormey@gmail.com>
2015-03-06 12:29:02 +0000

    can now create appointment when click on free slot in appointment list

--- src/ie/teamchile/smartapp/CreateAppointmentActivity.java
+++ src/ie/teamchile/smartapp/CreateAppointmentActivity.java
@@ -3,8 +3,10 @@ package ie.teamchile.smartapp;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import utility.ClinicSingleton;
 import utility.ServiceUserSingleton;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -22,8 +24,9 @@ public class CreateAppointmentActivity extends Activity {
 	private String name, clinic, date, time, duration, priority, visitType;
 	private PostAppointment postAppt = new PostAppointment();
 	private AccessDBTable db = new AccessDBTable();
-	private String response;
+	private String response, clinicID;
 	private JSONObject jsonNew;
+	private ProgressDialog pd;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,58 +49,58 @@ public class CreateAppointmentActivity extends Activity {
             case R.id.btn_confirm_appointment:
             	name = userName.getText().toString();
             	clinic = appointmentClinic.getText().toString();
+            	clinicID = ClinicSingleton.getInstance().getIDFromName(clinic);
             	date = apptDate.getText().toString();
             	time = apptTime.getText().toString();
             	duration = apptDuration.getText().toString();
             	priority = apptPriority.getText().toString();
             	visitType = apptVisitType.getText().toString();
             	
-            	Log.d("appointment", "name: " + name + "\nclinic: " +  clinic + "\nDate: " + date + 
+            	Log.d("appointment", "name: " + name + "\nclinic: " +  clinic  + "\nclinic id: " + clinicID + "\nDate: " + date + 
             						 "\nTime: " + time + "\nDuration: " + duration + "\nPriority: " + priority +
             						 "\nVisit Type: " + visitType);
             	
-            	postAppt.postAppointment(name, clinic, date, time, 
-						   				 duration, priority, visitType);
+            	new LongOperation(CreateAppointmentActivity.this).execute("service_users?name=" + name);
             	break;
             }
         }
 	}
     private class LongOperation extends AsyncTask<String, Void, JSONObject> {
 		private Context context;
+		private JSONObject json;
 		
 		public LongOperation(Context context){
 			this.context = context;
 		}
 		@Override
 		protected void onPreExecute() {
-/*			pd = new ProgressDialog(context);
-            pd.setMessage("Fetching Information");
-            pd.show();*/
+			pd = new ProgressDialog(context);
+            pd.setMessage("Creating Appointment");
+            pd.show();
 		}
 		protected JSONObject doInBackground(String... params) {
-			Log.d("singleton", "in service users updateLocal doInBackground");
 			try {
-				response = db.accessDB(params[0]);
-				jsonNew = new JSONObject(response);
+				String dbQuery = db.accessDB(params[0]);
+
+				json = new JSONObject(dbQuery);
+
+				ServiceUserSingleton.getInstance().setPatientInfo(json);
+				String userID = ServiceUserSingleton.getInstance().getServiceUserID();
+				
+				postAppt.postAppointment(userID, clinicID, date, time, duration,
+						priority, visitType);
+
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
-			Log.d("singleton", "query = " + jsonNew);
-			return jsonNew;
+			return null;
 		}
 		@Override
 		protected void onProgressUpdate(Void... values) {
 		}
 		@Override
         protected void onPostExecute(JSONObject result) {
-			String hospitalNumber = ServiceUserSingleton.getInstance().getHospitalNumber();
-/*			String email = ServiceUserSingleton.getSingletonIntance().getEmail(result);
-			String mobile = ServiceUserSingleton.getSingletonIntance().getMobileNumber(result);;
-			intent.putExtra("hospitalNumber", hospitalNumber);
-			intent.putExtra("email", email);
-			intent.putExtra("mobile", mobile);
 			pd.dismiss();
-			startActivity(intent);	*/
         }
 	}
 }
