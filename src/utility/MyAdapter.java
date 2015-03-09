c4aa29b115f6f6a45fd731c15dd2702cd628aa6a
Virtu <barry@virtustudios.ie>
2015-03-09 13:23:58 +0000

    working on TodayAppointment functionality

--- src/utility/MyAdapter.java
+++ src/utility/MyAdapter.java
@@ -2,17 +2,16 @@ package utility;
 
 import ie.teamchile.smartapp.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
+import android.net.ParseException;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -52,29 +51,33 @@ public class MyAdapter extends BaseAdapter {
 		if(convertView == null) {
 			view = inflater.inflate(R.layout.row_layout, parent, false);
 			TextView text1 = (TextView)view.findViewById(R.id.name);
-			TextView text2 = (TextView)view.findViewById(R.id.latest_message);
+			TextView text2 = (TextView)view.findViewById(R.id.appt_type);
+			TextView text3 = (TextView)view.findViewById(R.id.appt_info);
 			ImageView iv = (ImageView)view.findViewById(R.drawable.ic_launcher);
-			Button button = (Button)view.findViewById(R.id.detailBtn);
-			button.setOnClickListener(new ButtonClickListener());
 			String id = getItem(position);
-			text1.setText("Appointment ID: " + id);
-			//String theId = AppointmentSingleton.getSingletonIntance().getClinicID(id);
-			//text2.setText();
+			text2.setText(AppointmentSingleton.getInstance().getVisitType(appointments.get(position)));
+			text1.setText(getReadableDate(AppointmentSingleton.getInstance().getDate(appointments).get(position)) + " at " + 
+					AppointmentSingleton.getInstance().getTime(appointments).get(position));
+			text3.setText(String.valueOf(UserSingleton.getUserSingleton().getID()));
+			String myId = String.valueOf(UserSingleton.getUserSingleton().getID());
+			ToastAlert ta = new ToastAlert(context, "Appointments for Service User: " + myId);
+
 		}else {
 			view = convertView;
 		}
 		return view;
-	}
+	}	
 	
-	private class ButtonClickListener implements OnClickListener {
+	private String getReadableDate(String date) {
+		SimpleDateFormat reqFormat = new SimpleDateFormat("dd MMM, yyyy", Locale.UK);
+		SimpleDateFormat currentFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.UK);
+		String reformattedStr = null;
+		try {
 
-		@Override
-		public void onClick(View v) {
-			int id = v.getId();
-			switch(id) {
-			case R.id.detailBtn :
-			}			
-		}		
+		     reformattedStr = reqFormat.format(currentFormat.parse(date));
+		} catch (java.text.ParseException e) {
+		    e.printStackTrace();
+		}
+		return reformattedStr;
 	}
-	
 }
