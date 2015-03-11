11dc90780a989a10d28d092d33f1bdf8e17e1781
dgormally <david.gormally@gmail.com>
2015-03-11 09:46:18 +0000

    All postnatal information added. Obstretric history view created and somefunctionality added.

--- src/ie/teamchile/smartapp/AnteNatalActivity.java
+++ src/ie/teamchile/smartapp/AnteNatalActivity.java
@@ -1,27 +1,21 @@
 package ie.teamchile.smartapp;
 
 
-import java.sql.Date;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import utility.ServiceUserSingleton;
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class AnteNatalActivity extends MenuInheritActivity {
 	private TextView ageAnte;
 	private TextView nameAntiNatal, gestationAntiNatal, parityAntiNatal
-	                ,deliveryTime, bloodGroup, rhesus, obstetricHistory;
+	                ,deliveryTime, bloodGroup, rhesus;
 	private ImageView userImage, postNatal;
+	private TableRow obstetricHistory;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +36,10 @@ public class AnteNatalActivity extends MenuInheritActivity {
 		deliveryTime = (TextView)findViewById(R.id.deliveryTime);
 		bloodGroup = (TextView)findViewById(R.id.blood_group);
 		rhesus = (TextView)findViewById(R.id.rhesus);
-		obstetricHistory = (TextView)findViewById(R.id.obstetric_history_ante_natal);
+		
+		obstetricHistory = (TableRow)findViewById(R.id.obstretic_history);
+	
+		obstetricHistory.setOnClickListener(new AntiNatalOptions());
 	
 		String name = ServiceUserSingleton.getInstance().getName();
 		parityAntiNatal.setText(ServiceUserSingleton.getInstance().getParity());
@@ -57,10 +54,15 @@ public class AnteNatalActivity extends MenuInheritActivity {
 		int anteNatalAge = aa.getAge(age);
 		String theAge = String.valueOf(anteNatalAge);
 
-		
 		ageAnte.setText(theAge);
 		nameAntiNatal.setText(name);
+
+		
+		//gestationAntiNatal.setText(getIntent().getStringExtra("gestation"));
+
+		parityAntiNatal.setText(ServiceUserSingleton.getInstance().getParity());
 	}
+
 
 	private class AntiNatalOptions implements View.OnClickListener {
 		public void onClick(View v) {
@@ -72,6 +74,10 @@ public class AnteNatalActivity extends MenuInheritActivity {
 			case R.id.post_natal_from_ante_natal:
 				Intent intent1 = new Intent(AnteNatalActivity.this, PostNatalActivity.class);
 				startActivity(intent1);
+				break;
+			case R.id.obstretic_history:
+				Intent intent2 = new Intent(AnteNatalActivity.this, ObstreticHistoryActivity.class);
+				startActivity(intent2);
 				break;
 			}
 		}
--- src/ie/teamchile/smartapp/ObstreticHistoryActivity.java
+++ src/ie/teamchile/smartapp/ObstreticHistoryActivity.java
@@ -1,15 +1,53 @@
 package ie.teamchile.smartapp;
 
-import android.app.Activity;
+import java.util.ArrayList;
+
+import utility.ServiceUserSingleton;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class ObstreticHistoryActivity extends Activity {
+  public class ObstreticHistoryActivity extends MenuInheritActivity {
+	  private ArrayList<String>items = new ArrayList<String>(); ;
+	  private Button enter;
+	  private EditText edit;
+	  private String item;
+	  private ListView list;
+	  private String obsHistory;
+	//private String time;
+	  	@Override
+	  	protected void onCreate(Bundle savedInstanceState) {
+	  		super.onCreate(savedInstanceState);
+	  		setContentView(R.layout.list_layout);
+	  		enter = (Button)findViewById(R.id.button1);
+	  		edit = (EditText)findViewById(R.id.editField);
+	  		
+	  		final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_obstretic_history, R.id.textViewItem, items);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_obstretic_history);
-	}
-}
+	  		list = (ListView)findViewById(R.id.notes);
+	  		list.setAdapter(adapter);
+	  		obsHistory = ServiceUserSingleton.getInstance().getObstreticHistory();
+	  		//time = ServiceUserSingleton.getInstance().getCreatedTime();
+	  		//items.add(time);
+	  		 items.add(obsHistory);
+	  	     enter.setOnClickListener(new View.OnClickListener() {
+	  		   
+	  		        @Override
+	  		        public void onClick(View v) {
+	  		            // TODO Auto-generated method stub
+	  		        	item = edit.getText().toString();
+	  		        	if (item != null && item.trim().length() > 0) {
+	  		        	adapter.notifyDataSetChanged();
+	  		            items.add(item);
+	  		            edit.setText(null);
+	  		        	}
+	          }
+	  		            //Toast.makeText(null, item, Toast.LENGTH_SHORT).show();//Problem here
+	  		        
+	  		    });
+	  	}
+  }
