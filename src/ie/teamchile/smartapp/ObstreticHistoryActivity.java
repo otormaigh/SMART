fd61df7b00bf600e8c5c0c8fb0f5eca6bf09dfec
dgormally <david.gormally@gmail.com>
2015-03-11 11:28:17 +0000

    updated version of obstetric history

--- src/ie/teamchile/smartapp/ObstreticHistoryActivity.java
+++ src/ie/teamchile/smartapp/ObstreticHistoryActivity.java
@@ -15,9 +15,9 @@ import android.widget.TextView;
 	  private ArrayList<String>items = new ArrayList<String>(); ;
 	  private Button enter;
 	  private EditText edit;
-	  private String item;
 	  private ListView list;
-	  private String obsHistory;
+	  private String obsHistory,item, Obsname, Obsdate;
+	  private TextView name, date;
 	//private String time;
 	  	@Override
 	  	protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +25,18 @@ import android.widget.TextView;
 	  		setContentView(R.layout.list_layout);
 	  		enter = (Button)findViewById(R.id.button1);
 	  		edit = (EditText)findViewById(R.id.editField);
-	  		
-	  		final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_obstretic_history, R.id.textViewItem, items);
+	  		name = (TextView)findViewById(R.id.obs_name);
+	  		date = (TextView)findViewById(R.id.obs_date);
+	  		final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_obstretic_history,
+	  				R.id.textViewItem, items);
 
 	  		list = (ListView)findViewById(R.id.notes);
 	  		list.setAdapter(adapter);
 	  		obsHistory = ServiceUserSingleton.getInstance().getObstreticHistory();
+	  		Obsname = ServiceUserSingleton.getInstance().getName();
+	  		//Obsdate = ServiceUserSingleton.getInstance().getCreatedTime();
+	  		//date.setText(Obsdate);
+	  		name.setText(Obsname);
 	  		//time = ServiceUserSingleton.getInstance().getCreatedTime();
 	  		//items.add(time);
 	  		 items.add(obsHistory);
