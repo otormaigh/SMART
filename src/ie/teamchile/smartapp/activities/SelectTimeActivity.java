6f32769464efd116deda478e1e5d5d2cb00d079b
dgormally <david.gormally@gmail.com>
2015-03-20 12:51:13 +0000

    The about text view links to the about section on the hospitals website

---
+++ src/ie/teamchile/smartapp/activities/SelectTimeActivity.java
@@ -0,0 +1,14 @@
+package ie.teamchile.smartapp.activities;
+
+import ie.teamchile.smartapp.R;
+import ie.teamchile.smartapp.R.layout;
+import android.os.Bundle;
+
+public class SelectTimeActivity extends MenuInheritActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_select_time);
+	}
+}
---
+++ src/ie/teamchile/smartapp/connecttodb/AccessDBTable.java
@@ -0,0 +1,63 @@
+package ie.teamchile.smartapp.connecttodb;
+
+import ie.teamchile.smartapp.enums.CredentialsEnum;
+import ie.teamchile.smartapp.utility.ServiceProviderSingleton;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class AccessDBTable {
+	private static String url = CredentialsEnum.URL.toString();
+	private static String apiKey = CredentialsEnum.API_KEY.toString();
+    private String token, table, inputLine;
+    private int responseCode;
+    private StringBuffer response;
+    private URL obj;
+    private HttpURLConnection con;
+    private BufferedReader in;
+
+	public JSONObject accessDB(String table){
+		token = ServiceProviderSingleton.getInstance().getToken();
+        this.table = table;
+		return accessDB();
+	}
+	private JSONObject accessDB() {
+		try {
+			obj = new URL(url + table);
+			con = (HttpURLConnection) obj.openConnection();
+			con.setRequestMethod("GET");
+
+			// add request header
+			con.setRequestProperty("Api-Key", apiKey);
+			con.setRequestProperty("Auth-Token", token);
+
+			responseCode = con.getResponseCode();
+			Log.d("MYLOG", "\nSending 'GET' request to URL : " + url + table);
+			Log.d("MYLOG", "Response Code from AccessDB: " + responseCode);
+
+			in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			try {
+				return new JSONObject(response.toString());
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
