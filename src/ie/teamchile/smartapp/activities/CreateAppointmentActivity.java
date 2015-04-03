97302883ff350f1875ab0792d41b04a3795c19ea
Elliot Tormey <elliot.tormey@gmail.com>
2015-04-03 13:08:52 +0100

    format visit priority spinner in create appointments

--- src/ie/teamchile/smartapp/activities/CreateAppointmentActivity.java
+++ src/ie/teamchile/smartapp/activities/CreateAppointmentActivity.java
@@ -5,6 +5,7 @@ import ie.teamchile.smartapp.connecttodb.AccessDBTable;
 import ie.teamchile.smartapp.connecttodb.PostAppointment;
 import ie.teamchile.smartapp.utility.AppointmentSingleton;
 import ie.teamchile.smartapp.utility.ClinicSingleton;
+import ie.teamchile.smartapp.utility.MyAdapter;
 import ie.teamchile.smartapp.utility.ServiceUserSingleton;
 import ie.teamchile.smartapp.utility.ToastAlert;
 
@@ -27,7 +28,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -49,6 +49,8 @@ public class CreateAppointmentActivity extends MenuInheritActivity {
 	private TextView textDate, textClinic, textVisitType;
 	private Button confirmAppointment, btnUserSearch;
 	private Spinner visitTimeSpinner, visitDurationSpinner, visitPrioritySpinner;
+	private ArrayAdapter<String> myArrayAdapter;
+	private ArrayAdapter<CharSequence> visitPriorityAdapter;
 	private String name, clinic, apptDate, time, duration, priority, visitType;
 	private PostAppointment postAppt = new PostAppointment();
 	private AccessDBTable db = new AccessDBTable();
@@ -70,8 +72,8 @@ public class CreateAppointmentActivity extends MenuInheritActivity {
 		setContentView(R.layout.activity_create_appointment);
 		
 		c = Calendar.getInstance();
-		myCalendar = Calendar.getInstance();
-		
+		myCalendar = Calendar.getInstance();		
+
         userName = (EditText)findViewById(R.id.edit_service_user);
         confirmAppointment = (Button) findViewById(R.id.btn_confirm_appointment);
         confirmAppointment.setOnClickListener(new ButtonClick());
@@ -81,8 +83,13 @@ public class CreateAppointmentActivity extends MenuInheritActivity {
         visitTimeSpinner.setOnItemSelectedListener(new MySpinnerOnItemSelectedListener());
         visitDurationSpinner = (Spinner) findViewById(R.id.visit_duration_spinner);
         visitDurationSpinner.setOnItemSelectedListener(new MySpinnerOnItemSelectedListener());
+        
         visitPrioritySpinner = (Spinner) findViewById(R.id.visit_priority_spinner);
         visitPrioritySpinner.setOnItemSelectedListener(new MySpinnerOnItemSelectedListener());
+        visitPriorityAdapter = ArrayAdapter.createFromResource(this, R.array.visit_priority_list, R.layout.spinner_layout);
+        visitPriorityAdapter.setDropDownViewResource(R.layout.spinner_layout);
+        visitPrioritySpinner.setAdapter(visitPriorityAdapter);
+	    
         textVisitType = (TextView) findViewById(R.id.text_visit_type);
         
         textDate = (TextView)findViewById(R.id.visit_date_text);
@@ -173,19 +180,64 @@ public class CreateAppointmentActivity extends MenuInheritActivity {
             	Log.d("appointment", "name: " + name + "\nclinic: " +  clinic  + "\nclinic id: " + clinicID + "\nDate: " + apptDate + 
             						 "\nTime: " + time + "\nDuration: " + duration + "\nPriority: " + priority +
             						 "\nVisit Type: " + visitType);
-            	
-            	if(!name.isEmpty() && visitPrioritySpinner.getSelectedItemPosition() != 0) {
-            		new LongOperation(CreateAppointmentActivity.this).execute("service_users?name=" + name, "appointments");
-            	}else {
-            		ToastAlert ta = new ToastAlert(CreateAppointmentActivity.this, "Cannot proceed, \nSome fields are empty!", true);
-            	}
-            	       	
             	Log.d("postAppointment", "clinicID: " + clinicID);
             	Log.d("postAppointment", "clinicName: " + ClinicSingleton.getInstance().getClinicName(String.valueOf(clinicID)));
             	break;            
-            
+            case R.id.btn_user_search:
+            	//buildeAlertDialog();
+            	break;
             } 
         }
+	}
+	
+	private void buildeAlertDialog(List<String> searchResults){  
+    	searchResults.add("nore saturn");
+    	searchResults.add("shannon mercury");
+    	searchResults.add("nore saturn");
+    	searchResults.add("liffey neptune");
+    	searchResults.add("nore saturn");
+    	searchResults.add("shannon mercury");
+    	searchResults.add("nore saturn");
+    	searchResults.add("liffey neptune");
+    	searchResults.add("nore saturn");
+    	searchResults.add("shannon mercury");
+    	searchResults.add("nore saturn");
+    	searchResults.add("liffey neptune");
+    	searchResults.add("nore saturn");
+    	searchResults.add("shannon mercury");
+    	searchResults.add("nore saturn");
+    	searchResults.add("liffey neptune");
+    	searchResults.add("nore saturn");
+    	searchResults.add("shannon mercury");
+    	searchResults.add("nore saturn");
+    	searchResults.add("liffey neptune");
+    	searchResults.add("nore saturn");
+    	searchResults.add("shannon mercury");
+    	searchResults.add("nore saturn");
+    	searchResults.add("liffey neptune");
+    	searchResults.add("nore saturn");
+    	searchResults.add("shannon mercury");
+    	searchResults.add("nore saturn");
+    	searchResults.add("liffey neptune");
+    	
+    	if(!name.isEmpty() && visitPrioritySpinner.getSelectedItemPosition() != 0) {
+    		new LongOperation(CreateAppointmentActivity.this).execute("service_users?name=" + name, "appointments");
+    	}else {
+    		ToastAlert ta = new ToastAlert(CreateAppointmentActivity.this, "Cannot proceed, \nSome fields are empty!", true);
+    	}
+    	    	
+		LayoutInflater inflater = getLayoutInflater();
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(CreateAppointmentActivity.this);
+		View convertView = (View) inflater.inflate(R.layout.list, null);
+		ListView list = (ListView) convertView.findViewById(R.id.list_dialog);
+		
+		list.setOnItemClickListener(new onItemListener());
+		
+		alertDialog.setView(convertView);
+		alertDialog.setTitle("List");
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(CreateAppointmentActivity.this, android.R.layout.simple_list_item_1, searchResults);
+		list.setAdapter(adapter);
+		alertDialog.show();
 	}
 	
     private class LongOperation extends AsyncTask<String, Void, Boolean> {
@@ -194,9 +246,7 @@ public class CreateAppointmentActivity extends MenuInheritActivity {
 		private JSONArray query;
 		private Boolean userFound;
 		
-		public LongOperation(Context context){
-			this.context = context;
-		}
+		public LongOperation(Context context){ this.context = context; }
 		@Override
 		protected void onPreExecute() {
 			pd = new ProgressDialog(context);
@@ -211,8 +261,6 @@ public class CreateAppointmentActivity extends MenuInheritActivity {
 			if(!prefs.getBoolean("reuse", false)){
 				json = db.accessDB(params[0]);
 				ServiceUserSingleton.getInstance().setPatientInfo(json);
-				Log.d("bugs", "json: " + json);
-				Log.d("bugs", "singleton size: " + ServiceUserSingleton.getInstance().getUserID().size());
 				if(ServiceUserSingleton.getInstance().getUserID().size() > 0){
 					Log.d("bugs", "user found");
 					userID = ServiceUserSingleton.getInstance().getUserID().get(0);
@@ -260,7 +308,7 @@ public class CreateAppointmentActivity extends MenuInheritActivity {
                 	case 0: 
                 		break;
                 	default:
-                	duration = durationList.get(position);
+                		duration = durationList.get(position);
                 	break;
                 	}
                 case R.id.visit_priority_spinner:
@@ -271,6 +319,10 @@ public class CreateAppointmentActivity extends MenuInheritActivity {
                     case 1:
                     	priority = "scheduled";
                     	//Scheduled
+                    	break;
+                    case 2:
+                    	priority = "drop-in";
+                    	//Drop-In
                     	break;
                     }
                     break;
@@ -298,4 +350,70 @@ public class CreateAppointmentActivity extends MenuInheritActivity {
 			}
 		}
 	}
+	
+	/*private class LongOperation extends AsyncTask<String, Void, JSONObject> {
+		private Context context;
+
+		public LongOperation(Context context) {
+			this.context = context;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			pd = new ProgressDialog(context);
+			pd.setMessage("Fetching Information");
+			pd.show();
+		}
+
+		protected JSONObject doInBackground(String... params) {
+			Log.d("MYLOG", "ServiceUserSearch DoInBackground");
+			json = dbTable.accessDB(params[0]);
+			return json;
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+			Log.d("MYLOG", "On progress update");
+		}
+
+		@Override
+		protected void onPostExecute(JSONObject result) {
+			Log.d("MYLOG", "onPostExecute");
+			Log.d("bugs", "Result from on post " + result);
+
+			
+			 * if result from database is empty (check if null) toast to say no
+			 * query found if not empty do getSinglton.getName set this to
+			 * button text
+			 
+			try {
+				if (intent != null) {
+					ServiceUserSingleton.getInstance().setPatientInfo(result);
+					startActivity(intent);
+				} else {
+					searchResults.clear();
+					hospitalNumberList.clear();
+					if (result.getJSONArray("service_users").length() != 0) {
+						for (int i = 0; i < result.getJSONArray("service_users").length(); i++) {
+							ServiceUserSingleton.getInstance().setPatientInfo(result);
+							String name = ServiceUserSingleton.getInstance().getUserName().get(i);
+							String hospitalNumber = ServiceUserSingleton.getInstance().getUserHospitalNumber().get(i);
+							String dob = ServiceUserSingleton.getInstance().getUserDOB().get(i);
+							
+							searchResults.add(name + " - " + hospitalNumber + " - " + dob);
+							hospitalNumberList.add(hospitalNumber);
+							Log.d("bugs", "searchResults: " + searchResults);
+						}
+						createResultList(searchResults);	
+						adapter.notifyDataSetChanged();
+					} else {
+						Toast.makeText(getApplicationContext(), "No search results found", Toast.LENGTH_SHORT).show();
+					}
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			pd.dismiss();
+		}
+	}*/
 }
