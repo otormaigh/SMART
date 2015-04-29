02ebccc630fcddbae938662696462e13a8a8acec
elliot.laptop <elliot.devstream@gmail.com>
2015-04-29 09:49:34 +0100

    fix bug at logout changing activity before logout complete

--- src/ie/teamchile/smartapp/activities/MenuInheritActivity.java
+++ src/ie/teamchile/smartapp/activities/MenuInheritActivity.java
@@ -6,9 +6,11 @@ import ie.teamchile.smartapp.utility.AppointmentSingleton;
 import ie.teamchile.smartapp.utility.ServiceProviderSingleton;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,6 +20,8 @@ import android.widget.Toast;
 public class MenuInheritActivity extends Activity {
 
 	private Logout logout = new Logout();
+	private CountDownTimer timer;
+	private ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,21 +50,35 @@ public class MenuInheritActivity extends Activity {
                 public void onClick(DialogInterface dialoginterface, int i) {
                 	
                 }}).setPositiveButton(R.string.Yes, new DialogInterface.OnClickListener() {
-                	Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                     public void onClick(DialogInterface dialoginterface, int i) {
                         Log.d("MYLOG", "Logout button pressed");
+                        final Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    	intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                        				Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                        				Intent.FLAG_ACTIVITY_NEW_TASK);
                         if (ServiceProviderSingleton.getInstance().isLoggedIn() == false) {                            
-                        	intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
-		                            		Intent.FLAG_ACTIVITY_CLEAR_TASK |
-		                            		Intent.FLAG_ACTIVITY_NEW_TASK);
                             startActivity(intent);
                         } else {
                             logout.doLogout(ServiceProviderSingleton.getInstance().getToken());
-                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
-                                    		Intent.FLAG_ACTIVITY_CLEAR_TASK |
-                                    		Intent.FLAG_ACTIVITY_NEW_TASK);
-                            Toast.makeText(getApplicationContext(), "You are now logged out", Toast.LENGTH_SHORT).show();
-                            startActivity(intent);
+                            pd = new ProgressDialog(MenuInheritActivity.this);
+                			pd.setMessage("Logging Out");
+                			pd.show();
+                            
+                            timer = new CountDownTimer(1000, 1000){
+								@Override
+								public void onFinish() {
+									if(logout.getIsLoggedOut()){
+										Toast.makeText(getApplicationContext(), 
+												"You are now logged out", 
+												Toast.LENGTH_SHORT).show();
+										startActivity(intent);
+										pd.dismiss();
+									} else timer.start();
+								}
+								@Override
+								public void onTick(long millisUntilFinished) {
+								}
+                            }.start();
                         }
                     }
                 }).show();
