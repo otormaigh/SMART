86e679731ff2803a13729d3f49999ad13175bc87
elliot.laptop <elliot.devstream@gmail.com>
2015-05-08 14:49:19 +0100

    add landscape table view for parity details

--- src/ie/teamchile/smartapp/activities/ParityDetailsActivity.java
+++ src/ie/teamchile/smartapp/activities/ParityDetailsActivity.java
@@ -3,8 +3,13 @@ package ie.teamchile.smartapp.activities;
 import ie.teamchile.smartapp.R;
 import ie.teamchile.smartapp.utility.ServiceUserSingleton;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import android.app.Activity;
 import android.content.Context;
@@ -26,12 +31,45 @@ public class ParityDetailsActivity extends MenuInheritActivity {
 	private TextView name, parity, babyName;
 	private String patientName, patientParity;
 	private String sex_male = "ale", sex_female = "emale";
+	private List<String> nameBaby, hospitalNumber, dobBaby, genderBaby, gestationBaby,
+						 weightBaby, birthMode, birthOutcome, dobStr;
+	private ListView parityList;
+	private int orientation;
+	private DateFormat sdfMonthFullName = new SimpleDateFormat("dd MMMM yyyy", Locale.getDefault());
+	private DateFormat sdfDateTime = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.getDefault());
+	private DateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
 	
 	ServiceUserActivity ab = new ServiceUserActivity();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_parity_details);
+		parityList = (ListView)findViewById(R.id.parity_list);
+		
+		patientName = ServiceUserSingleton.getInstance().getUserName().get(0);
+		patientParity = ServiceUserSingleton.getInstance().getUserParity().get(0);
+		setTitle(patientName);
+		
+		nameBaby = ServiceUserSingleton.getInstance().getBabyName();
+		hospitalNumber = ServiceUserSingleton.getInstance().getBabyHospitalNumber();
+        dobBaby = ServiceUserSingleton.getInstance().getBabyDeliveryDateTime();
+		genderBaby = ServiceUserSingleton.getInstance().getBabyGender();
+		gestationBaby = ServiceUserSingleton.getInstance().getPregnancyGestation();
+		weightBaby = ServiceUserSingleton.getInstance().getBabyWeight();
+		birthMode = ServiceUserSingleton.getInstance().getPregnancyBirthMode();
+		birthOutcome = ServiceUserSingleton.getInstance().getBabyBirthOutcome();
+        dobStr = new ArrayList<String>();
+        
+        Log.d("bugs", "gestation list oncreate = " + gestationBaby.toString());
+		
+		for(int i = 0; i < dobBaby.size(); i++){
+			dobStr.add(dobBaby.get(i) + "\n");
+		}
+		  
+		for(int i = 0; i < weightBaby.size(); i ++){
+			String kg = getGramsToKg(Integer.parseInt(weightBaby.get(i)));
+			weightBaby.set(i, kg);
+		}
 		
 		switch(getScreenOrientation()){
 		case 1:				//ORIENTATION_PORTRAIT
@@ -61,53 +99,29 @@ public class ParityDetailsActivity extends MenuInheritActivity {
 	             orientation = Configuration.ORIENTATION_LANDSCAPE;
 	        }
 	    }
+	    this.orientation = orientation;
 	    return orientation;
 	}
 	
 	private void portraitCode(){
-		ListView parityList = (ListView)findViewById(R.id.parity_list);
 		babyName = (TextView)findViewById(R.id.baby_name_parity);
 		parity =(TextView)findViewById(R.id.parity_info);
 		
-		patientName = ServiceUserSingleton.getInstance().getUserName().get(0);
-		patientParity = ServiceUserSingleton.getInstance().getUserParity().get(0);
-		setTitle(patientName);
 		parity.setText(patientParity);
 		
-		List<String> nameBaby = ServiceUserSingleton.getInstance().getBabyName();
-		List<String> hospitalNumber = ServiceUserSingleton.getInstance().getBabyHospitalNumber();
-        
-		List<String> dobBaby = ServiceUserSingleton.getInstance().getBabyDeliveryDateTime();
-		List<String> genderBaby = ServiceUserSingleton.getInstance().getBabyGender();
-		List<String> gestationBaby = ServiceUserSingleton.getInstance().getPregnancyGestation();
-		
-		List<String> weightBaby = ServiceUserSingleton.getInstance().getBabyWeight();
-		Log.d("bugs", "weightBaby" + weightBaby);
-	
-		List<String> birthMode = ServiceUserSingleton.getInstance().getPregnancyBirthMode();
-		List<String> birthOutcome = ServiceUserSingleton.getInstance().getBabyBirthOutcome();
-        
-	    ArrayList<String> dobStr = new ArrayList<String>();
-	   
-		for(int i = 0; i < dobBaby.size(); i++){
-			dobStr.add(dobBaby.get(i) + "\n");
-		}
-		  
-		for(int i = 0; i < weightBaby.size(); i ++){
-			String kg = getGramsToKg(Integer.parseInt(weightBaby.get(i)));
-			weightBaby.set(i, kg);
-		}
-		
-        //textBabyDOB.setText(dobStr);
-		adapter = new ListElementAdapter(ParityDetailsActivity.this, nameBaby, hospitalNumber,
-				 dobStr, genderBaby, gestationBaby
-				,weightBaby, birthMode, birthOutcome);
+		adapter = new PortraitListAdapter(ParityDetailsActivity.this, 
+				 orientation, nameBaby, hospitalNumber, dobStr, 
+				 genderBaby, gestationBaby, weightBaby, 
+				 birthMode, birthOutcome);
 		parityList.setAdapter(adapter);
         //listView.setOnItemClickListener(new OnItemListener());		
 	}
 	
 	private void landscapeCode(){
-		
+		adapter = new PortraitListAdapter(ParityDetailsActivity.this, 
+				 orientation, dobStr, genderBaby, 
+				 gestationBaby, weightBaby, birthMode);
+		parityList.setAdapter(adapter);
 	}
 	
     public String getGramsToKg(int grams){
@@ -116,36 +130,52 @@ public class ParityDetailsActivity extends MenuInheritActivity {
     	return String.valueOf(kg);
     }
 	
-	private class ListElementAdapter extends BaseAdapter {
+	private class PortraitListAdapter extends BaseAdapter {
 		Context context;
-		int position;
+		int position, orientation;
 		LayoutInflater layoutInflater;
-		List<String> babyInformation, hospitalNumber, babyGender, gestation,
+		List<String> babyDob, hospitalNumber, babyGender, gestation,
 				 	 weight, name, birthMode, birthOutcome; 	
 
 		@Override
 		public void notifyDataSetChanged() {
 			super.notifyDataSetChanged();
 		}
-		public ListElementAdapter(Context context, List<String> name, List<String>hospitalNumber, 
-				List<String>babyInformation, List<String>babyGender, List<String>gestation,
-				List<String>weight, List<String>birthMode, List<String>birthOutcome) {
+		public PortraitListAdapter(Context context, int orientation, List<String> name,  
+				List<String>hospitalNumber, List<String>babyDob, List<String>babyGender, 
+				List<String>gestation, List<String>weight, List<String>birthMode, List<String>birthOutcome) {
 			super();
 			
 			this.context = context;
-			this.babyInformation = babyInformation;
-			this.hospitalNumber= hospitalNumber;
-			this.name=name;
+			this.orientation = orientation;
+			this.babyDob = babyDob;
+			this.hospitalNumber = hospitalNumber;
+			this.name = name;
 			this.babyGender = babyGender;
-			this.gestation= gestation;
-			this.weight=weight;
-			this.birthMode=birthMode;
-			this.birthOutcome=birthOutcome;
+			this.gestation = gestation;
+			this.weight = weight;
+			this.birthMode = birthMode;
+			this.birthOutcome = birthOutcome;
 			layoutInflater = LayoutInflater.from(context);
 		}
+		
+		public PortraitListAdapter(Context context, int orientation, List<String>babyDob, 
+				List<String>babyGender, List<String>gestation, List<String>weight, List<String>birthMode) {
+			super();
+			
+			this.context = context;
+			this.orientation = orientation;
+			this.babyDob = babyDob;
+			this.gestation = gestation;
+			this.babyGender = babyGender;
+			this.weight = weight;
+			this.birthMode = birthMode;
+			layoutInflater = LayoutInflater.from(context);
+		}
+		
 		@Override
 		public int getCount() {
-			return name.size();
+			return gestation.size();
 		}
 		@Override
 		public Object getItem(int position) {
@@ -158,33 +188,62 @@ public class ParityDetailsActivity extends MenuInheritActivity {
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			this.position = position;
-			convertView = layoutInflater.inflate(R.layout.parity_list, parent, false);
-			TextView nameOfBaby = (TextView)convertView.findViewById(R.id.baby_name_parity);
-			TextView hospitalNo = (TextView) convertView.findViewById(R.id.hospital_number_parity);
+			
+			convertView = layoutInflater.inflate(R.layout.parity_list_layout, parent, false);
 			TextView dobText = (TextView) convertView.findViewById(R.id.dob_parity);
 			TextView genderText = (TextView) convertView.findViewById(R.id.gender_parity);
 			TextView gestationText = (TextView) convertView.findViewById(R.id.gestation_parity);
 			TextView weightText = (TextView) convertView.findViewById(R.id.weight_parity);
 			TextView modeText = (TextView) convertView.findViewById(R.id.mode_of_birth_parity);
-			TextView birthOutcomeParity = (TextView) convertView.findViewById(R.id.birth_outcome_parity);
-			
-		    nameOfBaby.setText(name.get(position));
-			dobText.setText(ab.getDeliveryDate(babyInformation.get(position)));
-			hospitalNo.setText(hospitalNumber.get(position));
+
 			if(babyGender.get(position).equalsIgnoreCase("M")){
 				genderText.setText(babyGender.get(position) + sex_male);
 			}
 			else if (babyGender.get(position).equalsIgnoreCase("F")){
 				genderText.setText(babyGender.get(position) + sex_female);
 			}	
-			gestationText.setText(gestation.get(position));
-	
+			gestationText.setText(gestation.get(position));			
 			weightText.setText(weight.get(position));
 			modeText.setText(formatArrayString(birthMode.get(position)));
-			birthOutcomeParity.setText(birthOutcome.get(position));
-			//gestText.setText(gestationStr.get(position)); 
+			
+			switch(orientation){
+			case 1:				//ORIENTATION_PORTRAIT
+				TextView nameOfBaby = (TextView) convertView.findViewById(R.id.baby_name_parity);
+				TextView hospitalNo = (TextView) convertView.findViewById(R.id.hospital_number_parity);			
+				TextView birthOutcomeParity = (TextView) convertView.findViewById(R.id.birth_outcome_parity);
+				
+				nameOfBaby.setText(name.get(position));
+				hospitalNo.setText(hospitalNumber.get(position));
+				birthOutcomeParity.setText(birthOutcome.get(position));
+				dobText.setText(getDeliveryDate(babyDob.get(position), 0));
+				break;
+			case 2:				//ORIENTATION_LANDSCAPE
+				dobText.setText(getDeliveryDate(babyDob.get(position), 1));
+				break;
+			}			
+			
 			return convertView;			
 		}
+	}
+	
+	private String getDeliveryDate(String edd, int option) {
+		Date date = null;
+		String dateOfDevelivery = null;
+		try {
+			date = sdfDateTime.parse(edd);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		switch (option) {
+		case 0:
+			dateOfDevelivery = sdfMonthFullName.format(date);
+			break;
+		case 1:
+			dateOfDevelivery = sdfDate.format(date);
+			break;
+		}
+
+		return dateOfDevelivery;
 	}
 	
     private String formatArrayString(String toBeFormatted){
