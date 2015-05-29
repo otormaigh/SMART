830d4c5af1bea78c4f82397b9764a1d80760561e
Elliot Tormey <elliot.tormey@gmail.com>
2015-05-29 16:59:06 +0100

    add AppointmentTaken getter and setter to Error.java

--- app/src/main/java/ie/teamchile/smartapp/retrofit/Error.java
+++ app/src/main/java/ie/teamchile/smartapp/retrofit/Error.java
@@ -1,6 +1,7 @@
 package ie.teamchile.smartapp.retrofit;
 
 import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by user on 5/27/15.
@@ -8,6 +9,9 @@ import com.google.gson.annotations.Expose;
 public class Error {
     @Expose
     private String Error;
+    @SerializedName("Appointment taken")
+    @Expose
+    private String AppointmentTaken;
 
     /**
      * @return The Error
@@ -23,4 +27,17 @@ public class Error {
         this.Error = Error;
     }
 
+    /**
+     * @return The AppointmentTaken
+     */
+    public String getAppointmentTaken() {
+        return AppointmentTaken;
+    }
+
+    /**
+     * @param AppointmentTaken The Appointment taken
+     */
+    public void setAppointmentTaken(String AppointmentTaken) {
+        this.AppointmentTaken = AppointmentTaken;
+    }
 }
