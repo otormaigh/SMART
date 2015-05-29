e519223efcb32118407655a4d1ebc85df686c41d
Elliot Tormey <elliot.tormey@gmail.com>
2015-05-29 12:18:36 +0100

    add posting Appointment method to SmartApi and gson class to PostingData

--- app/src/main/java/ie/teamchile/smartapp/retrofit/PostingData.java
+++ app/src/main/java/ie/teamchile/smartapp/retrofit/PostingData.java
@@ -6,19 +6,53 @@ package ie.teamchile.smartapp.retrofit;
  */
 
 public class PostingData {
-   public Creds login;
+    public Login login;
+    public Appointment appointment;
 
-    public PostingData(String username, String password) {
-        this.login = new Creds(username, password);
+    public PostingData() {
     }
 
-    private class Creds {
+    public void postLogin(String username, String password) {
+        this.login = new Login(username, password);
+    }
+
+    public void postAppointment(String date, String time, int service_provider_id, int service_user_id,
+                                int clinic_id, String priority, String visit_type, String return_type) {
+
+        this.appointment = new Appointment(date, time, service_provider_id, service_user_id, clinic_id,
+                priority, visit_type, return_type);
+    }
+
+    private class Login {
         private String username;
         private String password;
 
-        public Creds(String username, String password) {
+        public Login(String username, String password) {
             this.username = username;
             this.password = password;
+        }
+    }
+
+    private class Appointment {
+        private String date;
+        private String time;
+        private int service_provider_id;
+        private int service_user_id;
+        private int clinic_id;
+        private String priority;
+        private String visit_type;
+        private String return_type;
+
+        public Appointment(String date, String time, int service_provider_id, int service_user_id,
+                           int clinic_id, String priority, String visit_type, String return_type){
+            this.date = date;
+            this.time = time;
+            this.service_provider_id = service_provider_id;
+            this.service_user_id = service_user_id;
+            this.clinic_id = clinic_id;
+            this.priority = priority;
+            this.visit_type = visit_type;
+            this.return_type = return_type;
         }
     }
 }
