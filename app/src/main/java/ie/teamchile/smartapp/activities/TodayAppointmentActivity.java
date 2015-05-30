05cd8048162b8498fe6545c0615498c35511af2e
Elliot Tormey <elliot.tormey@gmail.com>
2015-05-30 14:12:35 +0100

    move dateFormatter variables to BaseActivity

--- app/src/main/java/ie/teamchile/smartapp/activities/TodayAppointmentActivity.java
+++ app/src/main/java/ie/teamchile/smartapp/activities/TodayAppointmentActivity.java
@@ -1,74 +1,11 @@
 package ie.teamchile.smartapp.activities;
 
-/**
- * Today's Appointments for Service Provider
- * Barry Dempsey 10.03.15
- */
-
-import ie.teamchile.smartapp.connecttodb.AccessDBTable;
-import ie.teamchile.smartapp.utility.MyAdapter;
-import ie.teamchile.smartapp.utility.ToastAlert;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Locale;
-
-import org.json.JSONObject;
-
-import android.app.ListActivity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
-
-public class TodayAppointmentActivity extends ListActivity   {
-	private ArrayList<String>values;
-	private ArrayList<String>addresses = new ArrayList<String>();
-	private Calendar cal;
-	private String address;
-	private String date;
-	
+public class TodayAppointmentActivity extends BaseActivity   {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		new LongOperation().execute("service_users/2");
-		cal = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.UK);
-		date = sdf.format(cal.getTime());
-		values = new ArrayList<String>();
-	}
-	
-	private class LongOperation extends AsyncTask<String, Void, JSONObject> {
-			
-		@Override
-		protected JSONObject doInBackground(String... params) {
-			AccessDBTable access = new AccessDBTable();
-			JSONObject json = access.accessDB(params[0]);
-			return json;
-		}
-
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			ToastAlert ta = new ToastAlert(TodayAppointmentActivity.this, "Processing. . . ", false);
-		}
-
-		@Override
-		protected void onPostExecute(JSONObject result) {
-			super.onPostExecute(result);
-
-			if(values == null || values.size() == 0) {
-				values = new ArrayList<String>();
-				values.add("No appointments today");
-			}
-			MyAdapter adapter = new MyAdapter(TodayAppointmentActivity.this, values);
-			setListAdapter(adapter);
-		}
-
-		@Override
-		protected void onProgressUpdate(Void... values) {
-			super.onProgressUpdate(values);
-		}			
 	}
 }
