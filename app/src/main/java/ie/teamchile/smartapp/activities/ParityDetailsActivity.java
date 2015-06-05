6c5198191c77c6a98858048f6987211f7afb0a7c
Elliot Tormey <elliot.tormey@gmail.com>
2015-06-05 13:03:41 +0100

    add recycler view and horizontal scroll to landscape parity details

--- app/src/main/java/ie/teamchile/smartapp/activities/ParityDetailsActivity.java
+++ app/src/main/java/ie/teamchile/smartapp/activities/ParityDetailsActivity.java
@@ -14,6 +14,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
@@ -38,20 +40,38 @@ public class ParityDetailsActivity extends BaseActivity {
 	private List<String> birthMode = new ArrayList<>();
 	private List<String> birthOutcome = new ArrayList<>();
 	private List<String> dobStr = new ArrayList<>();
+	private List<String> hearing = new ArrayList<>();
+	private List<String> nbstList = new ArrayList<>();
+	private List<String> vitKList = new ArrayList<>();
 	private ListView parityList;
 	private int orientation;
+	private RecyclerView rvRecycler;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentForNav(R.layout.activity_parity_details);
 		parityList = (ListView)findViewById(R.id.parity_list);
+		rvRecycler = (RecyclerView) findViewById(R.id.rv_recycler);
 
 		patientName = ApiRootModel.getInstance().getServiceUsers().get(0).getPersonalFields().getName();
 		patientParity = ApiRootModel.getInstance().getServiceUsers().get(0).getClinicalFields().getParity();
 		setActionBarTitle(patientName + " (" + patientParity + ")");
 
 		List<Baby> babyList = ApiRootModel.getInstance().getBabies();
+
+		/*x 	 x name
+		 		 x birth_outcome
+				   delivery_date
+				 x gender
+				 x hearing
+				 x hospital_number
+				 x nbst
+				 x vitamin_k
+				 x weight (g)
+				 x gestation
+				 x mode of birth
+		*/
 
 		for(int i = 0; i < babyList.size(); i++){
 			nameBaby.add(babyList.get(i).getName());
@@ -62,6 +82,9 @@ public class ParityDetailsActivity extends BaseActivity {
 			weightBaby.add(babyList.get(i).getWeight());
 			birthMode.add(putArrayToString(ApiRootModel.getInstance().getPregnancies().get(i).getBirthMode()));
 			birthOutcome.add(babyList.get(i).getBirthOutcome());
+			hearing.add(babyList.get(i).getHearing());
+			nbstList.add(babyList.get(i).getNewbornScreeningTest());
+			vitKList.add(babyList.get(i).getVitaminK());
 		}
 
         dobStr = new ArrayList<>();
@@ -123,10 +146,16 @@ public class ParityDetailsActivity extends BaseActivity {
 	}
 	
 	private void landscapeCode(){
-		adapter = new ListAdapter(ParityDetailsActivity.this, 
+		/*adapter = new ListAdapter(ParityDetailsActivity.this,
 				 orientation, dobStr, genderBaby, gestationBaby,
 				weightBabyInKg, birthMode, birthOutcome);
-		parityList.setAdapter(adapter);
+		parityList.setAdapter(adapter);*/
+
+		LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+		rvRecycler.setLayoutManager(layoutManager);
+		RecyclerView.Adapter rvAdapter = new MyRecycleAdapter(nameBaby, hospitalNumber, dobBaby, genderBaby, gestationBaby,
+				weightBaby, birthMode, birthOutcome, hearing, nbstList, vitKList);
+		rvRecycler.setAdapter(rvAdapter);
 	}
 	
     public String getGramsToKg(int grams){
@@ -233,6 +262,95 @@ public class ParityDetailsActivity extends BaseActivity {
 			return convertView;			
 		}
 	}
+
+	private class MyRecycleAdapter extends RecyclerView.Adapter<MyRecycleAdapter.ViewHolder>{
+		private List<String> nameBaby, hospitalNumber, dobBaby, genderBaby, gestationBaby,
+				birthMode, birthOutcome, hearing, nbst, vitK;
+		private List<Integer> weightBaby;
+
+		public MyRecycleAdapter(List<String> nameBaby,
+								List<String> hospitalNumber,
+								List<String> dobBaby,
+								List<String> genderBaby,
+								List<String> gestationBaby,
+								List<Integer> weightBaby,
+								List<String> birthMode,
+								List<String> birthOutcome,
+								List<String> hearing,
+								List<String> nbst,
+								List<String> vitK){
+			this.nameBaby = nameBaby;
+			this.hospitalNumber = hospitalNumber;
+			this.dobBaby = dobBaby;
+			this.genderBaby = genderBaby;
+			this.gestationBaby = gestationBaby;
+			this.weightBaby = weightBaby;
+			this.birthMode = birthMode;
+			this.birthOutcome = birthOutcome;
+			this.hearing = hearing;
+			this.nbst = nbst;
+			this.vitK = vitK;
+		}
+
+		public class ViewHolder extends RecyclerView.ViewHolder {
+			private TextView tvDob, tvGest, tvGender, tvWeight, tvMode, tvOutcome,
+						tvHearing, tvNbst, tvVitK, tvName, tvHospitalNum;
+			public ViewHolder(View view) {
+				super(view);
+
+				tvDob = (TextView) view.findViewById(R.id.dob_parity);
+				tvGest = (TextView) view.findViewById(R.id.gestation_parity);
+				tvGender = (TextView) view.findViewById(R.id.gender_parity);
+				tvWeight = (TextView) view.findViewById(R.id.weight_parity);
+				tvMode = (TextView) view.findViewById(R.id.mode_of_birth_parity);
+				tvOutcome = (TextView) view.findViewById(R.id.birth_outcome_parity);
+				tvName = (TextView) view.findViewById(R.id.name);
+				tvHospitalNum = (TextView) view.findViewById(R.id.hospital_number);
+				tvHearing = (TextView) view.findViewById(R.id.birth_hearing);
+				tvNbst = (TextView) view.findViewById(R.id.birth_nbst);
+				tvVitK = (TextView) view.findViewById(R.id.birth_vit_k);
+			}
+		}
+
+		@Override
+		public MyRecycleAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.parity_list_layout, parent, false);
+			ViewHolder myViewHolder = new ViewHolder(view);
+			return myViewHolder;
+		}
+
+		@Override
+		public void onBindViewHolder(MyRecycleAdapter.ViewHolder holder, int position) {
+			TextView tvDob = holder.tvDob;
+			TextView tvGest = holder.tvGest;
+			TextView tvGender = holder.tvGender;
+			TextView tvWeight = holder.tvWeight;
+			TextView tvMode = holder.tvMode;
+			TextView tvOutcome = holder.tvOutcome;
+			TextView tvName = holder.tvName;
+			TextView tvHospitalNum = holder.tvHospitalNum;
+			TextView tvHearing = holder.tvHearing;
+			TextView tvNbst = holder.tvNbst;
+			TextView tvVitK = holder.tvVitK;
+
+			tvDob.setText(getDeliveryDate(dobBaby.get(position), 0));
+			tvGest.setText(gestationBaby.get(position));
+			tvGender.setText(genderBaby.get(position));
+			tvWeight.setText(String.valueOf(weightBaby.get(position)));
+			tvMode.setText(birthMode.get(position));
+			tvOutcome.setText(birthOutcome.get(position));
+			tvName.setText(nameBaby.get(position));
+			tvHospitalNum.setText(hospitalNumber.get(position));
+			tvHearing.setText(hearing.get(position));
+			tvNbst.setText(nbst.get(position));
+			tvVitK.setText(vitK.get(position));
+		}
+
+		@Override
+		public int getItemCount() {
+			return hospitalNumber.size();
+		}
+	}
 	
 	private String getDeliveryDate(String edd, int option) {
 		Date date = null;
@@ -250,7 +368,6 @@ public class ParityDetailsActivity extends BaseActivity {
 			dateOfDevelivery = dfDateOnly.format(date);
 			break;
 		}
-
 		return dateOfDevelivery;
 	}
 	
