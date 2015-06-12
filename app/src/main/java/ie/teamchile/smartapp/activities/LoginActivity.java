d66097c73078e819df80cd811bcf60fe415e50f1
Elliot Tormey <elliot.tormey@gmail.com>
2015-06-12 11:49:50 +0100

    fix bug with button style not showing at Login on API <21

--- app/src/main/java/ie/teamchile/smartapp/activities/LoginActivity.java
+++ app/src/main/java/ie/teamchile/smartapp/activities/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,7 @@ import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
 	private String username, password;
 	private Button btnLogin;
 	private TextView tvUsername, tvPassword;
