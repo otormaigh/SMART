28cc9ebc554a21aa6e74c65e40913f642fe67b39
elliot <elliot.tormey@gmail.com>
2015-06-17 17:11:15 +0100

    add PregnancyNotesActivity

---
+++ app/src/main/java/ie/teamchile/smartapp/activities/PregnancyNotesActivity.java
@@ -0,0 +1,14 @@
+package ie.teamchile.smartapp.activities;
+
+import android.os.Bundle;
+
+import ie.teamchile.smartapp.R;
+
+public class PregnancyNotesActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentForNav(R.layout.activity_pregnancy_notes);
+    }
+}
