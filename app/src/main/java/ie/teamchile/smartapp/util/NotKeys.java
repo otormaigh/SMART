58799dac9777e573b36d73eb61c0b9d3bd7a7017
Elliot Tormey <elliot.tormey@gmail.com>
2015-07-01 14:46:48 +0100

    remove RestAdapter from AppointmentCalendar

--- app/src/main/java/ie/teamchile/smartapp/util/NotKeys.java
+++ app/src/main/java/ie/teamchile/smartapp/util/NotKeys.java
@@ -4,7 +4,7 @@ package ie.teamchile.smartapp.util;
  * Created by user on 7/1/15.
  */
 public interface NotKeys {
-    String BASE_URL = "";
+    String BASE_URL = "http://";
     String API_KEY = "";
     String USERNAME = "";
     String PASSWORD = "";
