e1c549ff6bb350a912c922f81d7393816805b178
Elliot Tormey <elliot.tormey@gmail.com>
2015-09-04 21:39:06 +0100

    add splash screen on first app launch

--- app/src/main/java/ie/teamchile/smartapp/activities/SplashScreenActivity.java
+++ app/src/main/java/ie/teamchile/smartapp/activities/SplashScreenActivity.java
@@ -4,21 +4,34 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
+import ie.teamchile.smartapp.R;
 import ie.teamchile.smartapp.model.BaseModel;
 import ie.teamchile.smartapp.util.CheckForRoot;
 
-public class SplashScreenActivity extends Activity {
+public class SplashScreenActivity extends Activity implements View.OnClickListener {
     private String rootMsg;
+    private Button btnYes;
+    private Button btnNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+        setContentView(R.layout.activity_splash_screen);
+
+        btnYes = (Button) findViewById(R.id.btn_yes);
+        btnYes.setOnClickListener(this);
+        btnNo = (Button) findViewById(R.id.btn_no);
+        btnNo.setOnClickListener(this);
 
         Log.d("isDeviceRooted", "isDeviceRooted() = " + new CheckForRoot().isDeviceRooted());
+    }
 
+    private void checkIfLoggedIn() {
         if (BaseModel.getInstance().getLoginStatus()) {
             Log.d("bugs", "logged in = " + BaseModel.getInstance().getLoginStatus());
             Intent intent = new Intent(SplashScreenActivity.this, QuickMenuActivity.class);
@@ -27,6 +40,18 @@ public class SplashScreenActivity extends Activity {
             Log.d("bugs", "logged in = " + BaseModel.getInstance().getLoginStatus());
             Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
             startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_yes:
+                checkIfLoggedIn();
+                break;
+            case R.id.btn_no:
+                finish();
+                break;
         }
     }
 }
