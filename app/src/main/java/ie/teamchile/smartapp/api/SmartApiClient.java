c62037233cc0db3219c86f7dca926bb2891d8bd6
Elliot Tormey <elliot.tormey@gmail.com>
2015-11-01 17:09:24 +0000

    update SmartApiClient to work with RealmList<RealmInteger>

--- app/src/main/java/ie/teamchile/smartapp/api/SmartApiClient.java
+++ app/src/main/java/ie/teamchile/smartapp/api/SmartApiClient.java
@@ -5,10 +5,18 @@ import com.google.gson.FieldAttributes;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
 
 import ie.teamchile.smartapp.BuildConfig;
 import ie.teamchile.smartapp.model.BaseModel;
+import ie.teamchile.smartapp.model.RealmInteger;
 import ie.teamchile.smartapp.util.NotKeys;
+import io.realm.RealmList;
 import io.realm.RealmObject;
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
@@ -38,6 +46,25 @@ public class SmartApiClient {
                     @Override
                     public boolean shouldSkipClass(Class<?> clazz) {
                         return false;
+                    }
+                })
+                .registerTypeAdapter(new TypeToken<RealmList<RealmInteger>>() {
+                }.getType(), new TypeAdapter<RealmList<RealmInteger>>() {
+
+                    @Override
+                    public void write(JsonWriter out, RealmList<RealmInteger> value) throws IOException {
+                        // Ignore
+                    }
+
+                    @Override
+                    public RealmList<RealmInteger> read(JsonReader in) throws IOException {
+                        RealmList<RealmInteger> list = new RealmList<>();
+                        in.beginArray();
+                        while (in.hasNext()) {
+                            list.add(new RealmInteger(in.nextInt()));
+                        }
+                        in.endArray();
+                        return list;
                     }
                 })
                 .create();
