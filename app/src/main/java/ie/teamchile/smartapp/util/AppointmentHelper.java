f8f13d42aac3b5ff01375e6a5cc0b4698ac74bbf
Elliot Tormey <elliot.tormey@gmail.com>
2015-11-01 20:14:17 +0000

    remove appointment hashmap creator in AppointmentHelper.java

--- app/src/main/java/ie/teamchile/smartapp/util/AppointmentHelper.java
+++ app/src/main/java/ie/teamchile/smartapp/util/AppointmentHelper.java
@@ -2,16 +2,11 @@ package ie.teamchile.smartapp.util;
 
 import android.util.Log;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import ie.teamchile.smartapp.activities.BaseActivity;
 import ie.teamchile.smartapp.api.SmartApiClient;
-import ie.teamchile.smartapp.model.Appointment;
 import ie.teamchile.smartapp.model.BaseModel;
 import io.realm.Realm;
 import retrofit.Callback;
@@ -62,8 +57,6 @@ public class AppointmentHelper extends BaseActivity {
                         realm.commitTransaction();
 
                         BaseActivity.apptDone++;
-                        if (baseModel.getAppointments().size() > 0)
-                            addApptsToMaps(baseModel.getAppointments());
                     }
 
                     @Override
@@ -89,8 +82,6 @@ public class AppointmentHelper extends BaseActivity {
                         realm.commitTransaction();
 
                         BaseActivity.apptDone++;
-                        //if (baseModel.getAppointments().size() > 0)
-                        addApptsToMaps(baseModel.getAppointments());
                     }
 
                     @Override
@@ -100,63 +91,5 @@ public class AppointmentHelper extends BaseActivity {
                     }
                 }
         );
-    }
-
-    public void addApptsToMaps(List<Appointment> appointments) {
-        List<Integer> clinicApptIdList;
-        Map<String, List<Integer>> clinicVisitdateApptIdMap;
-        Map<Integer, Map<String, List<Integer>>> clinicVisitClinicDateApptIdMap =
-                BaseModel.getInstance().getClinicVisitClinicDateApptIdMap();
-        Map<Integer, Appointment> clinicVisitIdApptMap =
-                BaseModel.getInstance().getClinicVisitIdApptMap();
-
-        List<Integer> homeApptIdList;
-        Map<String, List<Integer>> homeVisitdateApptIdMap;
-        Map<Integer, Map<String, List<Integer>>> homeVisitClinicDateApptIdMap =
-                BaseModel.getInstance().getHomeVisitOptionDateApptIdMap();
-        Map<Integer, Appointment> homeVisitIdApptMap =
-                BaseModel.getInstance().getHomeVisitIdApptMap();
-
-        for (Appointment appt : appointments) {
-            clinicApptIdList = new ArrayList<>();
-            homeApptIdList = new ArrayList<>();
-            clinicVisitdateApptIdMap = new HashMap<>();
-            homeVisitdateApptIdMap = new HashMap<>();
-            String apptDate = appt.getDate();
-            int apptId = appt.getId();
-            int clinicId = appt.getClinicId();
-            int serviceOptionId = 0;
-            if (appt.getServiceOptionIds().size() > 0)
-                serviceOptionId = appt.getServiceOptionIds().get(0).getValue();
-
-            if (appt.getPriority().equals("home-visit")) {
-                if (homeVisitClinicDateApptIdMap.get(serviceOptionId) != null) {
-                    homeVisitdateApptIdMap = homeVisitClinicDateApptIdMap.get(serviceOptionId);
-                    if (homeVisitdateApptIdMap.get(apptDate) != null)
-                        homeApptIdList = homeVisitdateApptIdMap.get(apptDate);
-                }
-                homeApptIdList.add(apptId);
-                homeVisitdateApptIdMap.put(apptDate, homeApptIdList);
-
-                homeVisitClinicDateApptIdMap.put(serviceOptionId, homeVisitdateApptIdMap);
-                homeVisitIdApptMap.put(apptId, appt);
-            } else {
-                if (clinicVisitClinicDateApptIdMap.get(clinicId) != null) {
-                    clinicVisitdateApptIdMap = clinicVisitClinicDateApptIdMap.get(clinicId);
-                    if (clinicVisitdateApptIdMap.get(apptDate) != null)
-                        clinicApptIdList = clinicVisitdateApptIdMap.get(apptDate);
-                }
-                clinicApptIdList.add(apptId);
-                clinicVisitdateApptIdMap.put(apptDate, clinicApptIdList);
-
-                clinicVisitClinicDateApptIdMap.put(clinicId, clinicVisitdateApptIdMap);
-                clinicVisitIdApptMap.put(apptId, appt);
-            }
-        }
-        BaseModel.getInstance().setClinicVisitClinicDateApptIdMap(clinicVisitClinicDateApptIdMap);
-        BaseModel.getInstance().setClinicVisitIdApptMap(clinicVisitIdApptMap);
-
-        BaseModel.getInstance().setHomeVisitOptionDateApptIdMap(homeVisitClinicDateApptIdMap);
-        BaseModel.getInstance().setHomeVisitIdApptMap(homeVisitIdApptMap);
     }
 }
